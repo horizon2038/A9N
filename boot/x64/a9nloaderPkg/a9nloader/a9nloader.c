023a5856edef24ff47c0bb8f4f63f7e606542b73
horizon2038 <rekka728@gmail.com>
2023-04-18 00:23:40 +0900

    fix
    fix address operator

--- boot/x64/a9nloaderPkg/a9nloader/a9nloader.c
+++ boot/x64/a9nloaderPkg/a9nloader/a9nloader.c
@@ -57,9 +57,9 @@ EFI_STATUS open_root_directory(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root
     efi_status = EFI_SUCCESS;
 
     efi_status = get_image(image_handle, &device_image);
-    Print(L"start_get_image\r\n");
+    Print(L"end_get_image\r\n");
     efi_status = get_root_file_system(image_handle, device_image->DeviceHandle, &file_system);
-    Print(L"start_get_root_file_system\r\n");
+    Print(L"end_get_root_file_system\r\n");
     efi_status = get_root_directory(file_system, root_directory);
     Print(L"end_open_root_directory\r\n");
     return efi_status;
@@ -75,7 +75,7 @@ EFI_STATUS get_image(EFI_HANDLE image_handle, EFI_LOADED_IMAGE_PROTOCOL **device
 EFI_STATUS get_root_file_system(EFI_HANDLE image_handle, EFI_HANDLE device_handle, EFI_SIMPLE_FILE_SYSTEM_PROTOCOL **file_system)
 {
     EFI_STATUS efi_status;
-    efi_status = gBS->OpenProtocol(device_handle, &gEfiSimpleFileSystemProtocolGuid, (VOID**)file_system, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+    efi_status = gBS->OpenProtocol(device_handle, &gEfiSimpleFileSystemProtocolGuid, (VOID**)&file_system, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
     return handle_error(efi_status);
 }
 
