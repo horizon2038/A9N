5c68c3170ee199d9ab343e5bf1b1b5916b218e70
horizon2038 <rekka728@gmail.com>
2023-04-17 23:38:19 +0900

    delete
    delete unused variable

--- boot/x64/a9nloaderPkg/a9nloader/a9nloader.c
+++ boot/x64/a9nloaderPkg/a9nloader/a9nloader.c
@@ -15,7 +15,7 @@
 // PROTOTYPE
 void print_info(EFI_SYSTEM_TABLE*);
 void print_success(EFI_SYSTEM_TABLE*);
-void open_root_directory(EFI_HANDLE, EFI_SYSTEM_TABLE *system_table, EFI_FILE_PROTOCOL**);
+EFI_STATUS open_root_directory(EFI_HANDLE, EFI_SYSTEM_TABLE *system_table, EFI_FILE_PROTOCOL**);
 EFI_STATUS get_image(EFI_HANDLE, EFI_LOADED_IMAGE_PROTOCOL**);
 EFI_STATUS get_root_file_system(EFI_HANDLE, EFI_HANDLE, EFI_SIMPLE_FILE_SYSTEM_PROTOCOL**);
 EFI_STATUS get_root_directory(EFI_SIMPLE_FILE_SYSTEM_PROTOCOL*, EFI_FILE_PROTOCOL**);
@@ -24,26 +24,21 @@ EFI_STATUS handle_error(EFI_STATUS);
 
 EFI_STATUS EFIAPI efi_main (IN EFI_HANDLE image_handle, IN EFI_SYSTEM_TABLE *system_table)
 {
+    EFI_STATUS status;
     // EFI_LOADED_IMAGE_PROTOCOL* kernel_image; kernel file (*.elf)
     EFI_FILE_PROTOCOL *root_directory;
     EFI_FILE_PROTOCOL *kernel;
     UINT16 path[] = u"EFI\\BOOT\\BOOTX64.EFI";
     // system_table->ConOut->ClearScreen(system_table->ConOut);
     print_info(system_table);
-    open_root_directory(image_handle, system_table, &root_directory);
+    status = open_root_directory(image_handle, system_table, &root_directory);
 
     root_directory->Open(root_directory, &kernel, path, EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY);
     EFI_FILE_INFO file_info;
     UINT64 file_size;
-    EFI_STATUS status;
-
     file_size = sizeof(file_info);
     status = kernel->GetInfo(kernel, &gEfiFileInfoGuid, &file_size, (VOID*)&file_info);
-
-    CHAR8 buffer[256];
-    // AsciiSPrint(buffer, sizeof(buffer), "file_name: %s\nfile_size: %llu (%llu on disk) bytes\n", file_info.FileName, file_info.FileSize, file_info.PhysicalSize);
     Print(L"file_name: %s\nfile_size: %llu (%llu on disk) bytes\n", file_info.FileName, file_info.FileSize, file_info.PhysicalSize);
-    system_table->ConOut->OutputString(system_table->ConOut, L"a9nloader\n");
 
     while(1);
     return 0;
@@ -55,15 +50,16 @@ void print_info(EFI_SYSTEM_TABLE *system_table)
     system_table->ConOut->OutputString(system_table->ConOut, L"a9nloader\n");
 }
 
-void open_root_directory(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table, EFI_FILE_PROTOCOL **root_directory)
+EFI_STATUS open_root_directory(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table, EFI_FILE_PROTOCOL **root_directory)
 {
     EFI_LOADED_IMAGE_PROTOCOL *device_image;
-    EFI_STATUS efi_status;
     EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *file_system;
+    EFI_STATUS efi_status;
 
     efi_status = get_image(image_handle, &device_image);
     efi_status = get_root_file_system(image_handle, device_image->DeviceHandle, &file_system);
     efi_status = get_root_directory(file_system, root_directory);
+    return efi_status;
 }
 
 EFI_STATUS get_image(EFI_HANDLE image_handle, EFI_LOADED_IMAGE_PROTOCOL **device_image)
