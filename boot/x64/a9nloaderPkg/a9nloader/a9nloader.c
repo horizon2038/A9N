868e634d5b707e6d4158feecabb07607af8e093b
horizon <rekka728@gmail.com>
2023-04-21 20:48:28 +0900

    remove
    remove some ConOut method

--- boot/x64/a9nloaderPkg/a9nloader/a9nloader.c
+++ boot/x64/a9nloaderPkg/a9nloader/a9nloader.c
@@ -10,11 +10,11 @@
 #include <Protocol/DiskIo2.h>
 #include <Protocol/BlockIo.h>
 #include <Guid/FileInfo.h>
+
 #include "a9nloader.h"
+#include "elf.h"
 
 // PROTOTYPE
-void print_info(EFI_SYSTEM_TABLE*);
-void print_success(EFI_SYSTEM_TABLE*);
 EFI_STATUS open_root_directory(EFI_HANDLE, EFI_FILE_PROTOCOL**);
 EFI_STATUS get_image(EFI_HANDLE, EFI_LOADED_IMAGE_PROTOCOL**);
 EFI_STATUS get_root_file_system(EFI_HANDLE, EFI_HANDLE, EFI_SIMPLE_FILE_SYSTEM_PROTOCOL**);
@@ -24,11 +24,10 @@ EFI_STATUS handle_error(EFI_STATUS);
 
 EFI_STATUS EFIAPI efi_main (IN EFI_HANDLE image_handle, IN EFI_SYSTEM_TABLE *system_table)
 {
-    Print(L"start_uefi_main\r\n");
+    Print(L"start_efi_main\r\n");
     EFI_STATUS efi_status;
     EFI_FILE_PROTOCOL *root_directory;
     EFI_FILE_PROTOCOL *kernel;
-    print_info(system_table);
     efi_status = open_root_directory(image_handle, &root_directory);
 
     root_directory->Open(root_directory, &kernel, L"EFI\\BOOT\\BOOTX64.EFI", EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY);
@@ -40,12 +39,6 @@ EFI_STATUS EFIAPI efi_main (IN EFI_HANDLE image_handle, IN EFI_SYSTEM_TABLE *sys
 
     while(1);
     return efi_status;
-}
-
-void print_info(EFI_SYSTEM_TABLE *system_table)
-{
-    // system_table->ConOut->ClearScreen(system_table->ConOut);
-    system_table->ConOut->OutputString(system_table->ConOut, L"test_a9nloader\r\n");
 }
 
 EFI_STATUS open_root_directory(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root_directory)
@@ -94,9 +87,4 @@ EFI_STATUS handle_error(EFI_STATUS efi_status)
         return efi_status;
     }
     return EFI_SUCCESS;
-}
-
-void print_success(EFI_SYSTEM_TABLE *system_table)
-{
-    system_table->ConOut->OutputString(system_table->ConOut, L"SUCCESS\n");
 }
