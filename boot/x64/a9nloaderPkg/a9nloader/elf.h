7392acb6e22b87ff75fb3a19af3c64fd9efdce33
horizon2038 <rekka728@gmail.com>
2023-04-21 22:47:21 +0900

    change
    change member name in elf64_header and elf64_program_header

--- boot/x64/a9nloaderPkg/a9nloader/elf.h
+++ boot/x64/a9nloaderPkg/a9nloader/elf.h
@@ -3,9 +3,9 @@
 
 #include <stdint.h>
 
-typedef uint64_t elf64_addr;
+typedef uint64_t elf64_address;
 typedef uint16_t elf64_half;
-typedef uint64_t elf64_off;
+typedef uint64_t elf64_offset;
 typedef int32_t elf64_sword;
 typedef int64_t elf64_sxword;
 typedef uint32_t elf64_word;
@@ -14,21 +14,21 @@ typedef uint64_t elf64_xword;
 
 typedef struct
 {
-    unsigned char	e_ident[16];
-	elf64_half	e_type;
-	elf64_half	e_machine;
-	elf64_word	e_version;
-	elf64_addr	e_entry;
-	elf64_off	e_phoff;
-	elf64_off	e_shoff;
-	elf64_word	e_flags;
-	elf64_half	e_ehsize;
-	elf64_half	e_phentsize;
-	elf64_half	e_phnum;
-	elf64_half	e_shentsize;
-	elf64_half	e_shnum;
-	elf64_half	e_shstrndx;
-} elf64_ehdr;
+    unsigned char identifier[16];
+    elf64_half type;
+    elf64_half machine;
+    elf64_word version;
+    elf64_address entry_point_address;
+    elf64_offset program_header_offset;
+    elf64_offset section_header_offset;
+    elf64_word flags;
+    elf64_half size;
+    elf64_half program_header_size;
+    elf64_half program_header_number;
+    elf64_half section_header_size;
+    elf64_half section_header_number;
+    elf64_half section_header_string_table_index;
+} elf64_header;
 
 #define	ET_NONE 0
 #define	ET_REL 1
@@ -38,15 +38,15 @@ typedef struct
 
 typedef struct
 {
-    elf64_word p_type;
-    elf64_word p_flags;
-    elf64_off p_offset;
-    elf64_addr p_vaddr;
-    elf64_addr p_paddr;
-    elf64_xword p_filesz;
-    elf64_xword p_memsz;
-    elf64_xword p_align;
-} elf64_phdr;
+    elf64_word type;
+    elf64_word flags;
+    elf64_offset offset;
+    elf64_address virtual_address;
+    elf64_address physical_addr;
+    elf64_xword file_size;
+    elf64_xword memory_size;
+    elf64_xword alignment;
+} elf64_program_header;
 
 #define PT_LOAD 1
 #define PT_DYNAMIC 2
