6c7d2f758161568755811709ce008fe25beea356
horizon <rekka728@gmail.com>
2023-05-21 16:26:47 +0900

    clean
    refactoring file

--- boot/x64/a9nloaderPkg/a9nloader/a9nloader.c
+++ boot/x64/a9nloaderPkg/a9nloader/a9nloader.c
@@ -13,7 +13,8 @@
 
 #include "a9nloader.h"
 #include "elf.h"
-#include "open_kernel.h"
+#include "kernel_opener.h"
+#include "file_info_logger.h"
 
 EFI_STATUS EFIAPI efi_main (IN EFI_HANDLE image_handle, IN EFI_SYSTEM_TABLE *system_table)
 {
@@ -22,13 +23,8 @@ EFI_STATUS EFIAPI efi_main (IN EFI_HANDLE image_handle, IN EFI_SYSTEM_TABLE *sys
     EFI_STATUS efi_status;
     EFI_FILE_PROTOCOL *root_directory;
     EFI_FILE_PROTOCOL *kernel;
-    efi_status = open_root_directory(image_handle, &root_directory);
-    root_directory->Open(root_directory, &kernel, u"kernel\\kernel.elf", EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY);
-    EFI_FILE_INFO file_info;
-    UINT64 file_size;
-    file_size = sizeof(file_info) + sizeof(CHAR16) * 16;
-    efi_status = kernel->GetInfo(kernel, &gEfiFileInfoGuid, &file_size, (VOID*)&file_info);
-    Print(L"file_name: %s\nfile_size: %llu bytes\nfile_physical_size: %llu bytes", file_info.FileName, (unsigned long long)file_info.FileSize, (unsigned long long)file_info.PhysicalSize);
+    efi_status = open_kernel(image_handle, &root_directory, &kernel);
+    efi_status = print_file_info(&kernel);
     while(1);
     return efi_status;
 }
