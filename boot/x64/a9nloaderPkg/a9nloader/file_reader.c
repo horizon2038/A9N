e772e6f24c3996157eaa8b31e47f56588f2737fc
horizon <rekka728@gmail.com>
2023-05-29 22:49:06 +0900

    fix
    fix read_file

--- boot/x64/a9nloaderPkg/a9nloader/file_reader.c
+++ boot/x64/a9nloaderPkg/a9nloader/file_reader.c
@@ -1,10 +1,15 @@
 #include "file_reader.h"
 
-EFI_STATUS read_file(EFI_FILE_PROTOCOL *file, uint64_t offset, int size, void **buffer)
+#include <Uefi.h>
+#include <Library/UefiLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+
+EFI_STATUS read_file(EFI_FILE_PROTOCOL *file, uint64_t offset, int size, void *buffer)
 {
     EFI_STATUS efi_status;
-    *buffer = AllocatePool(size);
+    // *buffer = AllocatePool(size); // fix: Allocate Method
+    efi_status = gBS->AllocatePool(EfiReservedMemoryType, size, buffer);
     efi_status = file->SetPosition(file, offset);
-    efi_status =  file->Read(file, &size, *buffer);
+    efi_status =  file->Read(file, (UINTN*)&size, buffer);
     return efi_status;
 }
--- boot/x64/a9nloaderPkg/a9nloader/kernel_loader.h
+++ boot/x64/a9nloaderPkg/a9nloader/kernel_loader.h
@@ -11,9 +11,9 @@
 // clear bss to zero.
 
 EFI_STATUS load_kernel(EFI_FILE_PROTOCOL *kernel, uint64_t offset);
-uint64_t calculate_file_size(EFI_FILE_PROTOCOL *file, uint64_t *file_size);
+// uint64_t calculate_file_size(EFI_FILE_PROTOCOL *file, uint64_t *file_size);
 EFI_STATUS calculate_elf_segment(elf64_header *elf64_header, EFI_PHYSICAL_ADDRESS image_base);
 EFI_STATUS load_elf_segment(elf64_header *header, elf64_program_header *program_header, EFI_PHYSICAL_ADDRESS segment_start);
-EFI_STATUS zero_clear(elf64_program_header *program_header, EFI_PHYSICAL_ADDRESS segment_start, uint16_t count);
+void zero_clear(elf64_program_header *program_header, EFI_PHYSICAL_ADDRESS segment_start, uint16_t count);
 
 #endif
