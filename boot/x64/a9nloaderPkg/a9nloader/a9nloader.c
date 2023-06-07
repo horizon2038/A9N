f628b740e55e8e6700bbdcd67281f11d2680d52b
horizon <rekka728@gmail.com>
2023-06-07 16:10:52 +0900

    clean
    refactoring error handling

--- boot/x64/a9nloaderPkg/a9nloader/a9nloader.c
+++ boot/x64/a9nloaderPkg/a9nloader/a9nloader.c
@@ -19,15 +19,22 @@
 
 EFI_STATUS EFIAPI efi_main (IN EFI_HANDLE image_handle, IN EFI_SYSTEM_TABLE *system_table)
 {
-    system_table->ConOut->ClearScreen(system_table->ConOut);
-    Print(L"a9nloader v0.0.1\r\n");
-    Print(L"start_efi_main\r\n");
-    EFI_STATUS efi_status;
     EFI_FILE_PROTOCOL *root_directory;
     EFI_FILE_PROTOCOL *kernel;
-    efi_status = open_kernel(image_handle, &root_directory, &kernel);
-    efi_status = print_file_info(&kernel);
-    efi_status = load_kernel(kernel, 0);
+    EFI_STATUS efi_status = EFI_SUCCESS;
+
+    system_table->ConOut->ClearScreen(system_table->ConOut);
+    // system_table->ConOut->SetAttribute(system_table->ConOut, EFI_BLUE);
+    Print(L"a9nloader v0.0.1\r\n");
+    Print(L"start_efi_main\r\n");
+
+    while(!EFI_ERROR(efi_status))
+    {
+        efi_status = open_kernel(image_handle, &root_directory, &kernel);
+        efi_status = print_file_info(&kernel);
+        efi_status = load_kernel(kernel, 0);
+        break;
+    }
     while(1);
     return efi_status;
 }
--- boot/x64/a9nloaderPkg/a9nloader/kernel_loader.c
+++ boot/x64/a9nloaderPkg/a9nloader/kernel_loader.c
@@ -12,61 +12,57 @@ EFI_STATUS load_elf_program_header(EFI_FILE_PROTOCOL*, elf64_header*, elf64_prog
 
 EFI_STATUS load_kernel(EFI_FILE_PROTOCOL *kernel, uint64_t offset)
 {
-    EFI_STATUS efi_status;
-    // elf64_header loaded_elf64_header = (elf64_header) {0};
+    EFI_STATUS efi_status = EFI_SUCCESS;
     elf64_header *loaded_elf64_header;
-    efi_status = load_elf_header(kernel, &loaded_elf64_header);
-    print_elf_info(loaded_elf64_header);
     elf64_program_header *loaded_elf64_program_header;
-    efi_status = load_elf_program_header(kernel, loaded_elf64_header, &loaded_elf64_program_header);
-    Print(L"program_header_table_physical_address: %llx\n", (*(loaded_elf64_program_header + 2)).physical_address);
-    Print(L"program_header_table_physical_address: %llx\n", (loaded_elf64_program_header[2]).physical_address);
-    efi_status = calculate_elf_segment(loaded_elf64_header, loaded_elf64_program_header, 0);
+
+    while(!EFI_ERROR(efi_status))
+    {
+        efi_status = load_elf_header(kernel, &loaded_elf64_header);
+        efi_status = load_elf_program_header(kernel, loaded_elf64_header, &loaded_elf64_program_header);
+        efi_status = load_elf_segment(loaded_elf64_header, loaded_elf64_program_header, 0);
+        break;
+    }
     return efi_status;
 }
 
 EFI_STATUS load_elf_header(EFI_FILE_PROTOCOL *kernel, elf64_header **header)
 {
-    EFI_STATUS efi_status;
-    // efi_status = read_file(kernel, 0, sizeof(elf64_header), (void*)header);
-    efi_status = read_file_fixed(kernel, 0, sizeof(elf64_header), (void**)header);
+    EFI_STATUS efi_status = EFI_SUCCESS;
+    efi_status = read_file(kernel, 0, sizeof(elf64_header), (void**)header);
+    print_elf_header_info(*header);
     return efi_status;
 }
 
 EFI_STATUS load_elf_program_header(EFI_FILE_PROTOCOL *kernel, elf64_header *header, elf64_program_header **program_header)
 {
-    EFI_STATUS efi_status;
-    // gBS->AllocatePool(EfiReservedMemoryType, sizeof(elf64_program_header) * header->program_header_number, (void*)program_header);
-    // efi_status = read_file(kernel, (uint64_t)header, sizeof(elf64_program_header) * header->program_header_number, (void*)program_header);
-    uint64_t program_header_table_size = header->program_header_size * header->program_header_number;
-    Print(L"program_header_table_size: %llx\n", program_header_table_size);
-    efi_status = read_file_fixed(kernel, header->program_header_offset, program_header_table_size, (void**)program_header);
+    EFI_STATUS efi_status = EFI_SUCCESS;
+    uint64_t program_header_table_size = sizeof(elf64_program_header) * header->program_header_number;
+    Print(L"");
+    efi_status = read_file(kernel, header->program_header_offset, program_header_table_size, (void**)program_header);
     return efi_status;
 }
 
-EFI_STATUS calculate_elf_segment(elf64_header *header, elf64_program_header *program_header_table, EFI_PHYSICAL_ADDRESS image_base)
+EFI_STATUS load_elf_segment(elf64_header *header, elf64_program_header *program_header_table, EFI_PHYSICAL_ADDRESS image_base)
 {
-    EFI_STATUS efi_status;
-    Print(L"start_calculate_elf_segment\n");
-    for (UINT64 i = 0; i < header->program_header_number; i++)
+    EFI_STATUS efi_status = EFI_SUCCESS;
+    for (UINT64 i = 0; i < header->program_header_number; ++i)
     {
-        Print(L"count: %d\n", i);
         if ((program_header_table[i]).type != PT_LOAD)
         {
-            Print(L"!PT_LOAD\r\n");
             continue;
         }
-        Print(L"PT_LOAD\n");
-        efi_status = load_elf_segment(header, (elf64_program_header*)&(program_header_table[i]));
+        print_elf_program_header_info(&(program_header_table[i]));
+        efi_status = locate_elf_segment(header, (elf64_program_header*)&(program_header_table[i]));
         zero_clear(&(program_header_table[i]));
     }
     return efi_status;
 }
 
-EFI_STATUS load_elf_segment(elf64_header *header, elf64_program_header *program_header)
+EFI_STATUS locate_elf_segment(elf64_header *header, elf64_program_header *program_header)
 {
-    Print(L"load_elf_segment\r\n");
-    EFI_STATUS efi_status;
+    EFI_STATUS efi_status = EFI_SUCCESS;
+
     efi_status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, EFI_SIZE_TO_PAGES(program_header->memory_size), (EFI_PHYSICAL_ADDRESS*)program_header->virtual_address);
     gBS->CopyMem((void *)program_header->physical_address, (void *)((UINTN)header + program_header->offset), program_header->file_size);
     return efi_status;
