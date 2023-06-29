0f572596d0978183f9f3328b32c68cd9767a5661
horizon <rekka728@gmail.com>
2023-06-30 02:36:38 +0900

    add
    add own stdint.h and use

---
+++ boot/x64/a9nloaderPkg/a9nloader/stdint.h
@@ -0,0 +1,13 @@
+#ifndef STDINT_H
+#define STDINT_H
+
+// signed
+typedef short int16_t;
+typedef int int32_t;
+typedef long long int int64_t;
+
+// unsigned
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long int uint64_t;
+#endif
