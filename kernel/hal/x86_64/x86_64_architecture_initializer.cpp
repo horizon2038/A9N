3e7550a8bce6423fd187566a11ea787ef8a03f4a
horizon <rekka728@gmail.com>
2023-07-04 22:33:42 +0900

    update
    update kernel initializer interface

---
+++ kernel/hal/x86_64/x86_64_architecture_initializer.cpp
@@ -0,0 +1,29 @@
+#include "x86_64_architecture_initializer.hpp"
+
+x86_64_architecture_initializer :: x86_64_architecture_initializer()
+{
+
+}
+
+x86_64_architecture_initializer :: ~x86_64_architecture_initializer()
+{
+
+}
+
+void x86_64_architecture_initializer :: init_interrupt()
+{
+
+}
+void x86_64_architecture_initializer :: init_memory()
+{
+
+}
+void x86_64_architecture_initializer :: init_timer()
+{
+
+}
+void x86_64_architecture_initializer :: init_serial_device()
+{
+
+}
+
---
+++ kernel/hal/x86_64/x86_64_architecture_initializer.hpp
@@ -0,0 +1,18 @@
+#ifndef X86_64_ARCHITECTURE_INITIALIZER
+#define X86_64_ARCHITECTURE_INITIALIZER
+
+#include "../include/architecture_initializer.h"
+
+
+class x86_64_architecture_initializer : architecture_initializer
+{
+    public:
+        x86_64_architecture_initializer() {};
+        ~x86_64_architecture_initializer() {};
+        void init_interrupt();
+        void init_memory();
+        void init_timer();
+        void init_serial_device();
+};
+
+#endif
