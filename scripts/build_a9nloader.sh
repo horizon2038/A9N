0e56ee86c32ca76a56f08b9a3ea6ecb8113a5d7d
horizon <rekka728@gmail.com>
2023-07-15 11:58:39 +0900

    clean
    refactoring folders

---
+++ scripts/build_a9nloader.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CURRENT=`dirname $0`
+
+cd $CURRENT
+rm -rf ../chain/$ARCH/edk2/a9nloaderPkg
+cp -r ../src/boot/a9nloaderPkg ../chain/$ARCH/edk2/a9nloaderPkg
+cd ../chain/$ARCH/edk2
+EDKDIR=`pwd`
+export PATH=$LLVMDIR:$PATH
+export EDK_TOOLS_PATH=$EDKDIR/BaseTools
+source ./edksetup.sh BaseTools
+build
