37d7899195d9cb615860a325a01d3b5150fbe9b7
horizon <horizon@horizon-mac-14.local>
2023-07-29 19:38:03 +0900

    update
    update gdt_initializer

---
+++ src/kernel/hal/x86_64/gdt_configurer.s
@@ -0,0 +1,25 @@
+global _load_gdt, _load_segment_register
+
+gdtr DW 0
+    DQ 0
+
+_load_gdt:
+    mov [gdtr], di
+    mov [gdtr + 2], rsi
+    lgdt [gdtr]
+    ret
+
+_load_segment_register:
+    push rdi
+    lea rax, [rel .reload_cs]
+    push rax
+    retfq
+
+.reload_cs
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    ret
