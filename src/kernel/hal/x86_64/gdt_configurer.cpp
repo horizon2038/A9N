81e85426b372b71a03b0a443a9354cd3d020397e
horizon <horizon@horizon-mac-14.local>
2023-08-08 15:21:40 +0900

    clean
    rename some file and some fix

--- src/kernel/hal/x86_64/gdt_configurer.cpp
+++ src/kernel/hal/x86_64/gdt_configurer.cpp
@@ -1,33 +1,33 @@
-#include "gdt_initializer.hpp"
+#include "gdt_configurer.hpp"
 #include <stdint.h>
 
 extern "C" void _load_gdt(uint16_t size, uint64_t *offset);
 extern "C" void _load_segment_register(uint16_t code_segment_register);
 
-gdt_initializer :: gdt_initializer()
+gdt_configurer :: gdt_configurer()
 {
     // none
 }
 
-gdt_initializer :: ~gdt_initializer()
+gdt_configurer :: ~gdt_configurer()
 {
     // none
 }
 
-void gdt_initializer :: init_gdt()
+void gdt_configurer :: init_gdt()
 {
     load_gdt();
     load_segment_register(0x08);
 }
 
-void gdt_initializer :: load_gdt()
+void gdt_configurer :: load_gdt()
 {
     uint16_t gdt_size = sizeof(gdt) - 1;
     uint64_t *gdt_address = (uint64_t*)gdt;
     _load_gdt(gdt_size, gdt_address);
 }
 
-void gdt_initializer :: load_segment_register(uint16_t code_segment_register)
+void gdt_configurer :: load_segment_register(uint16_t code_segment_register)
 {
     _load_segment_register(code_segment_register);
 }
