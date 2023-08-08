9af87a9ae59f9e3716e31f2e791b27244b9bb8ef
horizon <rekka728@gmail.com>
2023-08-08 11:24:56 +0900

    fix
    fix far return for setup gdt

--- src/kernel/gdt_initializer.cpp
+++ src/kernel/gdt_initializer.cpp
@@ -1,8 +1,8 @@
 #include "gdt_initializer.hpp"
 #include <stdint.h>
 
-extern "C" void _load_gdt();
-extern "C" void _load_segment_register();
+extern "C" void _load_gdt(uint16_t size, uint64_t *offset);
+extern "C" void _load_segment_register(uint16_t code_segment_register);
 
 gdt_initializer :: gdt_initializer()
 {
@@ -17,18 +17,18 @@ gdt_initializer :: ~gdt_initializer()
 void gdt_initializer :: init_gdt()
 {
     load_gdt();
-    load_segment_register();
+    load_segment_register(0x08);
 }
 
 void gdt_initializer :: load_gdt()
 {
-    uint64_t gdtr[2];
-    gdtr[0] = ((uint64_t)gdt << 16) | (sizeof(gdt) - 1);
-    gdtr[1] = ((uint64_t)gdt >> 48);
-    __asm__ volatile("lgdt %0" :: "m" (gdtr));
+    uint16_t gdt_size = sizeof(gdt) - 1;
+    uint64_t *gdt_address = gdt;
+    _load_gdt(gdt_size, gdt_address);
 }
 
-void gdt_initializer :: load_segment_register()
+void gdt_initializer :: load_segment_register(uint16_t code_segment_register)
 {
+    _load_segment_register(code_segment_register);
 }
 
