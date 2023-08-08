b90d9f9f24854eea0f72806f53b13cf9bdbfb382
horizon <horizon@horizon-mac-14.local>
2023-08-08 18:29:30 +0900

    clean
    rename file: gdt_configurer -> segment_configurer

--- src/kernel/hal/x86_64/segment_configurer.cpp
+++ src/kernel/hal/x86_64/segment_configurer.cpp
@@ -1,33 +1,33 @@
-#include "gdt_configurer.hpp"
+#include "segment_configurer.hpp"
 #include <stdint.h>
 
 extern "C" void _load_gdt(uint16_t size, uint64_t *offset);
 extern "C" void _load_segment_register(uint16_t code_segment_register);
 
-gdt_configurer :: gdt_configurer()
+segment_configurer :: segment_configurer()
 {
     // none
 }
 
-gdt_configurer :: ~gdt_configurer()
+segment_configurer :: ~segment_configurer()
 {
     // none
 }
 
-void gdt_configurer :: init_gdt()
+void segment_configurer :: init_gdt()
 {
     load_gdt();
     load_segment_register(0x08);
 }
 
-void gdt_configurer :: load_gdt()
+void segment_configurer :: load_gdt()
 {
     uint16_t gdt_size = sizeof(gdt) - 1;
     uint64_t *gdt_address = (uint64_t*)gdt;
     _load_gdt(gdt_size, gdt_address);
 }
 
-void gdt_configurer :: load_segment_register(uint16_t code_segment_register)
+void segment_configurer :: load_segment_register(uint16_t code_segment_register)
 {
     _load_segment_register(code_segment_register);
 }
--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -1,4 +1,4 @@
-#include <x86_64/gdt_configurer.hpp>
+#include <x86_64/segment_configurer.hpp>
 #include <stdint.h>
 #include <cpp_dependent/new.hpp>
 
@@ -7,9 +7,9 @@ extern "C" int kernel_main()
     // TODO: test gdt things
     // init gdt and set protection mode (ring 0 - ring 3, kernel/user mode).
     // first: make GDT struct and that array.
-    char buf[255];
-    gdt_configurer *my_gdt_configurer = new((void*)buf) gdt_configurer;
-    my_gdt_configurer->init_gdt();
+    char buf[8];
+    segment_configurer *my_segment_configurer = new((void*)buf) segment_configurer;
+    my_segment_configurer->init_gdt();
     __asm volatile("hlt");
     return 2038;
 }
