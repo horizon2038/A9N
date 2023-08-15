35b1cf2cb75286d640fdb46d080d3eb05870efeb
horizon <rekka728@gmail.com>
2023-08-15 21:49:23 +0900

    add
    interrupt impl x86_64

--- src/kernel/hal/include/interrupt.hpp
+++ src/kernel/hal/include/interrupt.hpp
@@ -2,12 +2,13 @@
 #define INTERRUPT_HPP
 
 #include <stdint.h>
+#include "interrupt_handler.hpp"
 
 class interrupt
 {
     public:
         virtual void init_interrupt() = 0;
-        virtual void register_interrupt(uint32_t irq_number, void *target_irq_handler) = 0;
+        virtual void register_interrupt(uint32_t irq_number, interrupt_handler &target_irq_handler) = 0;
         virtual void enable_interrupt(uint32_t irq_number) = 0;
         virtual void disable_interrupt(uint32_t irq_number) = 0;
         virtual void enable_interrupt_all() = 0;
