6fa499beddcc1cca2d562c2e7b796f4f9bc15f48
horizon <rekka728@gmail.com>
2023-09-04 01:02:42 +0900

    update
    move this ptr to private from public

--- src/hal/x86_64/pit_timer.hpp
+++ src/hal/x86_64/pit_timer.hpp
@@ -19,7 +19,6 @@ namespace hal::x86_64
             pit_timer(hal::interface::port_io &injected_port_io);
             ~pit_timer();
 
-            static pit_timer *this_timer;
             void init_timer() override;
             void configure_timer(uint16_t hz) override;
             void clock() override;
@@ -30,6 +29,7 @@ namespace hal::x86_64
             uint32_t ticks = 0;
 
         private:
+            static pit_timer *this_timer;
             hal::interface::port_io &_port_io;
             hal::x86_64::pic _pic;
     };
