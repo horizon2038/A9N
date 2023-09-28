1c412bca78dc2d2a15edcb060ce96c896afb46bd
horizon <rekka728@gmail.com>
2023-09-28 22:16:32 +0900

    remove
    remove context_switch.hpp/cpp

--- src/kernel/process_manager.hpp
+++ src/kernel/process_manager.hpp
@@ -9,13 +9,23 @@ namespace kernel
     class process_manager
     {
         public:
-            process_manager();
+            process_manager
+            (
+                hal::interface::process_manager &injected_process_manager,
+                hal::interface::interrupt &inject_interrupt
+            );
+
             ~process_manager();
 
             void create_process(const char *process_name, uint64_t process_address);
             void delete_process(int32_t process_id);
+            void switch_context(process *preview_process, process *next_process);
 
             static void schedule(); // called by timer-interrupt handler
+
+        private:
+            hal::interface::process_manager &_process_manager;
+            hal::interface::interrupt &_interrupt;
     };
 }
 
