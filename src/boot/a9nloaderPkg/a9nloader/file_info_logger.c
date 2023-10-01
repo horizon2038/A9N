f46ff1be9ce2d89367e345f1af91afa726e3df68
horizon <horizon@horizon-mac-14.local>
2023-10-01 16:44:11 +0900

    update
    update method to static in file_info_logger.c

--- src/boot/a9nloaderPkg/a9nloader/file_info_logger.c
+++ src/boot/a9nloaderPkg/a9nloader/file_info_logger.c
@@ -6,9 +6,9 @@
 #include <Guid/FileInfo.h>
 #include "stdint.h"
 
-uint64_t calculate_file_size();
-EFI_STATUS get_file_info(EFI_FILE_PROTOCOL **, uint64_t*, EFI_FILE_INFO*);
-void print_info(EFI_FILE_INFO*);
+static uint64_t calculate_file_size();
+static EFI_STATUS get_file_info(EFI_FILE_PROTOCOL **, uint64_t*, EFI_FILE_INFO*);
+static void print_info(EFI_FILE_INFO*);
 
 EFI_STATUS print_file_info(EFI_FILE_PROTOCOL **file)
 {
@@ -21,7 +21,7 @@ EFI_STATUS print_file_info(EFI_FILE_PROTOCOL **file)
     return efi_status;
 }
 
-uint64_t calculate_file_size()
+static uint64_t calculate_file_size()
 {
     uint64_t file_size;
     file_size = sizeof(EFI_FILE_INFO);
@@ -29,14 +29,14 @@ uint64_t calculate_file_size()
     return file_size;
 }
 
-EFI_STATUS get_file_info(EFI_FILE_PROTOCOL **file, uint64_t *file_size, EFI_FILE_INFO *file_info)
+static EFI_STATUS get_file_info(EFI_FILE_PROTOCOL **file, uint64_t *file_size, EFI_FILE_INFO *file_info)
 {
     EFI_STATUS efi_status;
     efi_status = (*file)->GetInfo(*file, &gEfiFileInfoGuid, file_size, (VOID*)file_info);
     return efi_status;
 }
 
-void print_info(EFI_FILE_INFO *file_info)
+static void print_info(EFI_FILE_INFO *file_info)
 {
     Print
     (
