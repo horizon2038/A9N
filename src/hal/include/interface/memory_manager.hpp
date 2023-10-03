4c318a9899b08344dc17c4b79a6fd47997b05abe
horizon <rekka728@gmail.com>
2023-10-03 20:43:02 +0900

    fix
    fix typo (interrput->interrupt)

---
+++ src/hal/include/interface/memory_manager.hpp
@@ -0,0 +1,14 @@
+#ifndef HAL_MEMORY_MANAGER_HPP
+#define HAL_MEMORY_MANAGER_HPP
+
+namespace hal::interface
+{
+    class memory_manager
+    {
+        public:
+            virtual void virtual_memory_map() = 0;
+            virtual void virtual_memory_unmap() = 0;
+    };
+}
+
+#endif
