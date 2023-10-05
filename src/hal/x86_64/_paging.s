bff4ade16831ea2595f453f0f83c62043d8b4165
horizon <rekka728@gmail.com>
2023-10-06 01:07:26 +0900

    fix
    fixed incorrect use of registers in _paging.s (eax -> rax)

--- src/hal/x86_64/_paging.s
+++ src/hal/x86_64/_paging.s
@@ -1,10 +1,10 @@
 global _load_cr3, _flush_tlb
 
-_load_cr3: ; void load_cr3(uint64_t address);
-    mov cr3. rdi
+_load_cr3: ; void load_cr3(uint64_t address()
+    mov cr3, rdi
     ret
 
-_flush_tlb: ; void flash_tlb();
-    mov eax, cr3
-    mov cr3, eax
+_flush_tlb: ; void flash_tlb()
+    mov rax, cr3
+    mov cr3, rax
 
