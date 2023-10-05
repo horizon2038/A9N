85e6b86abe41975df1653a9162ff7826537ea889
horizon <horizon@horizon-mac-14.local>
2023-10-05 18:01:25 +0900

    add
    add memory_manager for x86_64

---
+++ src/hal/x86_64/memory_manager.cpp
@@ -0,0 +1,16 @@
+#include "memory_manager.hpp"
+
+namespace hal::x86_64
+{
+    void memory_manager::init_memory()
+    {
+    }
+
+    void memory_manager::virtual_memory_map()
+    {
+    }
+
+    void memory_manager::virtual_memory_unmap()
+    {
+    }
+}
