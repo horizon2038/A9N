6e3e012a976631c07b20b74be7b26b366cbdcd3d
horizon <horizon@horizon-mac-14.local>
2023-10-06 15:44:35 +0900

    clean
    rename init_memory arg (page_table -> target_page_table)

--- src/hal/x86_64/memory_manager.cpp
+++ src/hal/x86_64/memory_manager.cpp
@@ -2,8 +2,10 @@
 
 namespace hal::x86_64
 {
-    void memory_manager::init_memory()
+    void memory_manager::init_memory(uint64_t target_page_table)
     {
+        // allocate page_table (pml4) ;
+        //
     }
 
     void memory_manager::virtual_memory_map()
