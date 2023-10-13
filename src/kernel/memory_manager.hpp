ef4bc4ec55deced23da9140dce24202e8b6183db
horizon <horizon@horizon-mac-14.local>
2023-10-13 16:31:47 +0900

    add
    add init_memory_block

--- src/kernel/memory_manager.hpp
+++ src/kernel/memory_manager.hpp
@@ -4,14 +4,33 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#include "boot_info.h"
+#include "memory_type.h"
 
-class memory_manager
+namespace kernel
 {
-    public:
-        memory_manager();
-        void init();
-        void *allocate_physical_memory(size_t size);
-};
+    constexpr static uint16_t PAGE_SIZE = 4096;
+
+    struct memory_block
+    {
+        uint64_t physical_address;
+        size_t size;
+        memory_block *next;
+    };
+
+    // future: delegate physical_memory_allocation_system to virtual_memory_server (user-space).
+    class memory_manager
+    {
+        public:
+            memory_manager(const memory_info &target_memory_info);
+            void *allocate_physical_memory(size_t size);
+            void dealocate_physical_memory(void *pointer);
+
+        private:
+            memory_block *head_memory_block;
+            void init(const memory_info &target_memory_info);
+            void init_memory_block(const memory_info &target_memory_info);
+    };
+}
 
 #endif
+
