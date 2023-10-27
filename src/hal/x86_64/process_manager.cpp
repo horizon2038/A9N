36d74747eaaab72ea96e598eaee6ffd6c82911e6
horizon <horizon@horizon-mac-14.local>
2023-10-28 07:24:57 +0900

    update
    update ipc

--- src/hal/x86_64/process_manager.cpp
+++ src/hal/x86_64/process_manager.cpp
@@ -19,19 +19,12 @@ namespace hal::x86_64
 
     void process_manager::switch_context(kernel::process *preview_process, kernel::process *next_process)
     {
-        /*
-        kernel::virtual_address *preview_stack_pointer = reinterpret_cast<kernel::virtual_address*>(preview_process->stack_pointer);
-        kernel::virtual_address *next_stack_pointer = reinterpret_cast<kernel::virtual_address*>(next_process->stack_pointer);
-        kernel::utility::logger::printk("current_sp : 0x%llx, next_sp : 0x%llx\n", reinterpret_cast<uint64_t>(preview_process->stack_pointer), reinterpret_cast<uint64_t>(next_process->stack_pointer));
-        // _switch_context(preview_stack_pointer, next_stack_pointer);
-        */
         _switch_context(&preview_process->stack_pointer, &next_process->stack_pointer);
     }
 
     void process_manager::create_process(kernel::process *target_process, kernel::virtual_address entry_point_address)
     {
         std::memset(&target_process->stack, 0, kernel::STACK_SIZE_MAX);
-        // target_process->stack_pointer = reinterpret_cast<kernel::virtual_address>(&target_process->stack) + sizeof(uint8_t) * kernel::STACK_SIZE_MAX;
         kernel::virtual_address *target_stack_pointer = reinterpret_cast<kernel::virtual_address*>(&target_process->stack[kernel::STACK_SIZE_MAX]);
 
         /*
@@ -45,7 +38,6 @@ namespace hal::x86_64
         rflags
         */
 
-        // kernel::virtual_address *target_stack_pointer = reinterpret_cast<kernel::virtual_address*>(target_process->stack_pointer);
         *--target_stack_pointer = entry_point_address;
         *--target_stack_pointer = 0;
         *--target_stack_pointer = 0;
@@ -61,7 +53,6 @@ namespace hal::x86_64
         kernel::utility::logger::printk("entry_point_address : %llx\n", entry_point_address);
         kernel::utility::logger::printk("in-stack entry_point_address : %llx\n", *target_stack_pointer);
 
-        // page_fault
         // target_process->stack_pointer = convert_virtual_to_physical_address(target_process->stack_pointer);
     }
 
---
+++ src/kernel/include/message.hpp
@@ -0,0 +1,14 @@
+#ifndef MESSAGE_HPP
+#define MESSAGE_HPP
+
+#include <stdint.h>
+
+struct message
+{
+    int32_t sender_process_id;
+    char messagge_type[256];
+    uint8_t data[1024];
+};
+
+#endif
+
