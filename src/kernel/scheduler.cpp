8ffa1d2a521420a475b61f9e6148b1ea225d0d79
horizon <rekka728@gmail.com>
2023-10-28 05:04:11 +0900

    clean
    delete unused comments

--- src/kernel/scheduler.cpp
+++ src/kernel/scheduler.cpp
@@ -18,40 +18,6 @@ namespace kernel
 
     process *scheduler::schedule_next_process()
     {
-        /*
-        utility::logger::printk("schedule_next_process\n");
-
-        process *target_process = &_process_list[current_process_index];
-
-        while(target_process->status != process_status::READY)
-        {
-            utility::logger::printk("loop: %d\n", current_process_index);
-            target_process = &_process_list[current_process_index];
-
-            utility::logger::printk("process_name: %s, process_id: %llu, process_entry: %llx\n", target_process->name, target_process->id, reinterpret_cast<virtual_address>(target_process->stack_pointer));
-
-            if (current_process_index > 512)
-            {
-                current_process_index = 0;
-                break;
-            }
-            current_process_index++;
-        }
-
-        if (target_process->status != process_status::READY)
-        {
-            utility::logger::printk("can't find schedulable process\n");
-            return nullptr;
-        }
-
-        target_process->status = process_status::RUNNING;
-        utility::logger::printk("set running\n");
-        _current_process = target_process;
-        utility::logger::printk("_current_process_index : %d\n", current_process_index);
-        _current_process->status = process_status::READY;
-        utility::logger::printk("set ready\n");
-        return target_process;
-        */
         utility::logger::printk("schedule_next_process\n");
 
         process *target_process = nullptr;
@@ -59,21 +25,17 @@ namespace kernel
 
         do
         {
-            // インデックスをインクリメントする前に512を超えているかチェック
             if (current_process_index >= 12)
             {
                 current_process_index = 0;
             }
 
             target_process = &_process_list[current_process_index];
-            // utility::logger::printk("loop: %d\n", current_process_index);
-            // utility::logger::printk("process_name: %s, process_id: %llu, process_entry: %llx\n", target_process->name, target_process->id, reinterpret_cast<virtual_address>(target_process->stack_pointer));
 
-            // インデックスをインクリメント
             current_process_index++;
 
         }
-        while(target_process->status != process_status::READY && current_process_index != start_index);  // 全プロセスをチェックするか、READY状態のプロセスを見つけるまでループ
+        while(target_process->status != process_status::READY && current_process_index != start_index);  
 
         if (target_process->status != process_status::READY)
         {
@@ -81,7 +43,6 @@ namespace kernel
             return nullptr;
         }
 
-        // 新しい_current_processを設定する前に前の_current_processのstatusをREADYに設定
         if (_current_process != nullptr)
         {
             _current_process->status = process_status::READY;
