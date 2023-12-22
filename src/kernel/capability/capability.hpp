ec89578e79d1ab8255a32686bf3da29fb514d82e
horizon <rekka728@gmail.com>
2023-12-22 18:34:48 +0900

    add
    add capability_type

---
+++ src/kernel/capability/capability.hpp
@@ -0,0 +1,33 @@
+#ifndef CAPABILITY_HPP
+#define CAPABILITY_HPP
+
+#include <common.hpp>
+#include <stdint.h>
+
+namespace kernel
+{
+    // currently, capabilities are provided in a hardware-independent manner.
+    // however, to make extensions in the hardware-dependent part easier,
+    // they take even values, following seL4.
+    enum class capability_type : uint8_t
+    {
+        UNINITIALIZED = 0,
+        GENERIC = 2,
+        CAPABILITY_NODE = 4,
+        PROCESS_CONTROL_BLOCK = 6,
+        FRAME = 8
+    };
+
+    struct capability
+    {
+        word_t data[2];
+    };
+
+    struct capability_pointer
+    {
+        capability_type type;
+        virtual_address capability_address;
+    };
+}
+
+#endif
