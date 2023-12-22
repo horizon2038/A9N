80199667479d43ec450c2fb0e11fb8a1f0dfc821
horizon <rekka728@gmail.com>
2023-12-22 19:20:02 +0900

    add
    add capability_descriptor

---
+++ src/library/include/capability/capability_descriptor.hpp
@@ -0,0 +1,11 @@
+#ifndef CAPABILITY_DESCRIPTOR_HPP
+#define CAPABILITY_DESCRIPTOR_HPP
+
+#include <common.hpp>
+
+namespace library::capability
+{
+    using capability_descriptor = kernel::word_t;
+}
+
+#endif
