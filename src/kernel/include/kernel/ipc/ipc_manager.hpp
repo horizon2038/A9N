daa7c680114e07b197ccbbe9dbc4b5ef34c7d799
horizon <horizon@horizon-mac-14.local>
2023-12-24 15:58:24 +0900

    clean
    kernel/ipc/* : fix include-directive to kernel/**

--- src/kernel/include/kernel/ipc/ipc_manager.hpp
+++ src/kernel/include/kernel/ipc/ipc_manager.hpp
@@ -1,21 +1,22 @@
 #ifndef IPC_HPP
 #define IPC_HPP
 
-#include <process/process.hpp>
-#include <ipc/message.hpp>
+#include <kernel/process/process.hpp>
+#include <kernel/ipc/message.hpp>
 
 namespace kernel
 {
     static constexpr int32_t ANY_PROCESS = -1;
+
     class ipc_manager
     {
-        public:
-            ipc_manager() {};
-            ~ipc_manager() {};
-            void send(process_id receiver_process_id, message *msg);
-            void receive(process_id sender_process_id, message *msg);
+      public:
+        ipc_manager() {};
+        ~ipc_manager() {};
 
-            // void notify();
+        void send(process_id receiver_process_id, message *msg);
+        void receive(process_id sender_process_id, message *msg);
+        // void notify();
     };
 }
 
--- src/kernel/include/kernel/ipc/message.hpp
+++ src/kernel/include/kernel/ipc/message.hpp
@@ -11,4 +11,3 @@ struct message
 };
 
 #endif
-
