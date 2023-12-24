fc42a1b7a64ca5ef637b96cc479c6310707d95ce
horizon <horizon@horizon-mac-14.local>
2023-12-24 15:52:04 +0900

    clean
    format by clang-format

--- src/hal/include/hal/interface/serial.hpp
+++ src/hal/include/hal/interface/serial.hpp
@@ -8,11 +8,11 @@ namespace hal::interface
 {
     class serial
     {
-        public:
-            virtual void init_serial(common::word baud_rate) = 0;
-            virtual uint8_t read_serial() = 0;
-            virtual void write_serial(char data) = 0;
-            virtual void write_string_serial(char *out) = 0;
+      public:
+        virtual void init_serial(common::word baud_rate) = 0;
+        virtual uint8_t read_serial() = 0;
+        virtual void write_serial(char data) = 0;
+        virtual void write_string_serial(char *out) = 0;
     };
 }
 
