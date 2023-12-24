c0c1a428661a6992cbe09de31a289c059e3d1bdd
horizon <horizon@horizon-mac-14.local>
2023-12-24 16:23:26 +0900

    clean
    fix include-directive to kernel/**

--- src/kernel/ipc_manager.cpp
+++ src/kernel/ipc_manager.cpp
@@ -1,4 +1,4 @@
-#include <ipc/ipc_manager.hpp>
+#include <kernel/ipc/ipc_manager.hpp>
 
 #include "kernel.hpp"
 #include <process/process.hpp>
@@ -10,20 +10,23 @@ namespace kernel
 {
     void ipc_manager::send(process_id receiver_process_id, message *msg)
     {
-        process *receiver_process =
-            kernel_object::process_manager->search_process_from_id(
-                receiver_process_id);
+        process *receiver_process
+            = kernel_object::process_manager->search_process_from_id(
+                receiver_process_id
+            );
         utility::logger::printk(
             "ipc_send : %s -> %s\n",
             kernel_object::process_manager->current_process->name,
-            receiver_process->name);
+            receiver_process->name
+        );
         utility::logger::printk(
             "message | type : %d | data : \e[32m%s\e[0m\n",
             msg->type,
-            reinterpret_cast<const char *>(msg->data));
+            reinterpret_cast<const char *>(msg->data)
+        );
 
-        process *current_process =
-            kernel_object::process_manager->current_process;
+        process *current_process
+            = kernel_object::process_manager->current_process;
         std::memcpy(&current_process->message_buffer, msg, sizeof(message));
 
         if (receiver_process == nullptr || receiver_process == current_process)
@@ -33,10 +36,10 @@ namespace kernel
             return;
         }
 
-        bool is_ready =
-            receiver_process->status == process_status::BLOCKED
-            && (receiver_process->receive_from == ANY_PROCESS
-                || receiver_process->receive_from == current_process->id);
+        bool is_ready
+            = receiver_process->status == process_status::BLOCKED
+           && (receiver_process->receive_from == ANY_PROCESS
+               || receiver_process->receive_from == current_process->id);
 
         if (!is_ready)
         {
@@ -50,7 +53,8 @@ namespace kernel
             std::memcpy(
                 &receiver_process->message_buffer,
                 &current_process->message_buffer,
-                sizeof(message));
+                sizeof(message)
+            );
 
             receiver_process->status = process_status::READY;
 
@@ -60,15 +64,15 @@ namespace kernel
 
     void ipc_manager::receive(process_id source_process_id, message *msg)
     {
-        process *current_process =
-            kernel_object::process_manager->current_process;
+        process *current_process
+            = kernel_object::process_manager->current_process;
         process *sender_process = nullptr;
 
         current_process->receive_from = source_process_id;
 
         // TODO: fix flag
-        bool dequeued =
-            current_process->send_wait_queue.dequeue(sender_process);
+        bool dequeued
+            = current_process->send_wait_queue.dequeue(sender_process);
 
         if (!dequeued
             || (source_process_id != ANY_PROCESS
