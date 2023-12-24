eefb4af04dc3ce80e146d8dc20fb0a2f7eee236b
horizon <horizon@horizon-mac-14.local>
2023-12-24 17:09:53 +0900

    clean
    utility : move directory

--- src/kernel/logger.cpp
+++ src/kernel/logger.cpp
@@ -1,4 +1,4 @@
-#include <library/logger.hpp>
+#include <kernel/utility/logger.hpp>
 
 namespace kernel::utility
 {
@@ -38,7 +38,8 @@ namespace kernel::utility
         this_logger->print_sender("ERROR", terminal_color::RED);
         this_logger->print_splitter();
         this_logger->_print.printf("%s\n", message);
-        while (true);
+        while (true)
+            ;
     }
 
     void logger::printk(const char *format, ...)
@@ -67,82 +68,89 @@ namespace kernel::utility
 
     void logger::a9nout()
     {
-        this_logger->_print.printf
-        (
-            "\e[32m" \
-            "                                   \n" \
-            "        d8888 .d8888b. 888b    888 \n" \
-            "       d88888d88P  Y88b8888b   888 \n" \
-            "      d88P888888    88888888b  888 \n" \
-            "     d88P 888Y88b. d888888Y88b 888 \n" \
-            "    d88P  888  Y888P888888 Y88b888 \n" \
-            "   d88P   888       888888  Y88888 \n" \
-            "  d8888888888Y88b  d88P888   Y8888 \n" \
-            " d88P     888  Y8888P  888    Y888 \n" \
-            "                                   \n" \
+        this_logger->_print.printf("\e[32m"
+                                   "                                   \n"
+                                   "        d8888 .d8888b. 888b    888 \n"
+                                   "       d88888d88P  Y88b8888b   888 \n"
+                                   "      d88P888888    88888888b  888 \n"
+                                   "     d88P 888Y88b. d888888Y88b 888 \n"
+                                   "    d88P  888  Y888P888888 Y88b888 \n"
+                                   "   d88P   888       888888  Y88888 \n"
+                                   "  d8888888888Y88b  d88P888   Y8888 \n"
+                                   " d88P     888  Y8888P  888    Y888 \n"
+                                   "                                   \n"
+                                   "\e[0m");
+
+        this_logger->_print.printf("\e[32m"
+                                   "                                   \n"
+                                   "         @@@@@@  @@@@@@@@@         \n"
+                                   "        @@@@@@@  @@@@@@@@@@        \n"
+                                   "        @@@@@@  @@@@@@@@@@@        \n"
+                                   "       @@@@@@@  @@@@@@@@@@@@       \n"
+                                   "       @@@@@@  @@@@@@ @@@@@@       \n"
+                                   "      @@@@@@@  @@@@@@ &@@@@@@      \n"
+                                   "      @@@@@@  @@@@@@   @@@@@@      \n"
+                                   "     @@@@@@@  @@@@@@   0@@@@@@     \n"
+                                   "     @@@@@@  @@@@@@  @  @@@@@@     \n"
+                                   "    @@@@@@&  @@@@@@  @@ &@@@@@@    \n"
+                                   "    @@@@@@  ......  @@@  @@@@@@    \n"
+                                   "   @@@@@@@  @@@@@@@@@@@@ &@@@@@@   \n"
+                                   "   @@@@@@  @@@@@@@@@@@@@  @@@@@@   \n"
+                                   "  @@@@@@@ .@@@@@@@@@@@@@0 (@@@@@@  \n"
+                                   "  @@@@@@  @@@@@@           @@@@@@  \n"
+                                   " @@@@@@0  @@@@@@           /@@@@@@ \n"
+                                   "                                   \n"
+                                   "\e[0m");
+
+        this_logger->_print.printf(
+            "\e[11F"
+            "\e[36G"
+            "\e[32m"
+            "kernel: \e[52G\e[37mA9N v0.0.1\n"
+            "\e[36G"
+            "\e[32m"
+            "architecture: \e[52G\e[37m%s\n"
+            "\e[36G"
+            "\e[32m"
+            "creator: \e[52G\e[37mhorizon2k38\n"
+            "\e[36G"
+            "\e[32m"
+            "project: \e[52G\e[37mmitoujr\n"
+            "\e[36G"
+            "\e[32m"
+            "about: \e[52G\e[37mA9N is a kernel built on HAL <Hardware "
+            "Abstraction Layer> and microkernel.\n"
+            "\e[36G"
+            "\e[32m"
+            "\e[52G\e[37mIt combines high portability, stability, and "
+            "scalability.\n"
             "\e[0m"
+            "\e[13E",
+            "x86_64" // TODO: change hard-coded architecture.
         );
-
-        this_logger->_print.printf
-        (
-            "\e[32m" \
-            "                                   \n" \
-            "         @@@@@@  @@@@@@@@@         \n" \
-            "        @@@@@@@  @@@@@@@@@@        \n" \
-            "        @@@@@@  @@@@@@@@@@@        \n" \
-            "       @@@@@@@  @@@@@@@@@@@@       \n" \
-            "       @@@@@@  @@@@@@ @@@@@@       \n" \
-            "      @@@@@@@  @@@@@@ &@@@@@@      \n" \
-            "      @@@@@@  @@@@@@   @@@@@@      \n" \
-            "     @@@@@@@  @@@@@@   0@@@@@@     \n" \
-            "     @@@@@@  @@@@@@  @  @@@@@@     \n" \
-            "    @@@@@@&  @@@@@@  @@ &@@@@@@    \n" \
-            "    @@@@@@  ......  @@@  @@@@@@    \n" \
-            "   @@@@@@@  @@@@@@@@@@@@ &@@@@@@   \n" \
-            "   @@@@@@  @@@@@@@@@@@@@  @@@@@@   \n" \
-            "  @@@@@@@ .@@@@@@@@@@@@@0 (@@@@@@  \n" \
-            "  @@@@@@  @@@@@@           @@@@@@  \n" \
-            " @@@@@@0  @@@@@@           /@@@@@@ \n" \
-            "                                   \n" \
-            "\e[0m"
-        );
-
-        this_logger->_print.printf
-        (
-            "\e[11F" \
-            "\e[36G" \
-            "\e[32m" \
-            "kernel: \e[52G\e[37mA9N v0.0.1\n" \
-            "\e[36G" \
-            "\e[32m" \
-            "architecture: \e[52G\e[37m%s\n" \
-            "\e[36G" \
-            "\e[32m" \
-            "creator: \e[52G\e[37mhorizon2k38\n" \
-            "\e[36G" \
-            "\e[32m" \
-            "project: \e[52G\e[37mmitoujr\n" \
-            "\e[36G" \
-            "\e[32m" \
-            "about: \e[52G\e[37mA9N is a kernel built on HAL <Hardware Abstraction Layer> and microkernel.\n" \
-            "\e[36G" \
-            "\e[32m" \
-            "\e[52G\e[37mIt combines high portability, stability, and scalability.\n" \
-            "\e[0m" \
-            "\e[13E", "x86_64" // TODO: change hard-coded architecture.
-        );
-
     }
 
     void logger::print_log_id(const char *color_id)
     {
-        this_logger->_print.printf("[ %s%s%010d%s ] ", terminal_color::RESET, color_id, log_id, terminal_color::RESET);
+        this_logger->_print.printf(
+            "[ %s%s%010d%s ] ",
+            terminal_color::RESET,
+            color_id,
+            log_id,
+            terminal_color::RESET
+        );
         log_id++;
     }
 
     void logger::print_sender(const char *sender, const char *color_id)
     {
-        this_logger->_print.printf("[ %s%s%8s%s ] ", terminal_color::RESET, color_id, sender, terminal_color::RESET);
+        this_logger->_print.printf(
+            "[ %s%s%8s%s ] ",
+            terminal_color::RESET,
+            color_id,
+            sender,
+            terminal_color::RESET
+        );
     }
 
     void logger::print_splitter()
@@ -152,30 +160,28 @@ namespace kernel::utility
 
     void logger::mitoujr()
     {
-        this_logger->_print.printf
-        (
-            "\e[32m" \
-            "                                                  \n" \
-            "                         /                        \n" \
-            "                       /////                      \n" \
-            "                      /// ///                     \n" \
-            "                    ///     ///                   \n" \
-            "                   ///       ///                  \n" \
-            "                 ////         ////                \n" \
-            "                ////           ////               \n" \
-            "              ////////       ////////             \n" \
-            "             ///    ///     ///    ///            \n" \
-            "            ///      ///   ///      ///           \n" \
-            "          ///          /////          ///         \n" \
-            "         ///            ///            ///        \n" \
-            "       ///            /// ///            ///      \n" \
-            "      ///            ///   ///            ///     \n" \
-            "    ////           ////     ////           ////   \n" \
-            "   ///            ///         ///            ///  \n" \
-            "  //////////////////           //////////////////(\n" \
-            "                                                  \n" \
-            "\e[0m" \
+        this_logger->_print.printf(
+            "\e[32m"
+            "                                                  \n"
+            "                         /                        \n"
+            "                       /////                      \n"
+            "                      /// ///                     \n"
+            "                    ///     ///                   \n"
+            "                   ///       ///                  \n"
+            "                 ////         ////                \n"
+            "                ////           ////               \n"
+            "              ////////       ////////             \n"
+            "             ///    ///     ///    ///            \n"
+            "            ///      ///   ///      ///           \n"
+            "          ///          /////          ///         \n"
+            "         ///            ///            ///        \n"
+            "       ///            /// ///            ///      \n"
+            "      ///            ///   ///            ///     \n"
+            "    ////           ////     ////           ////   \n"
+            "   ///            ///         ///            ///  \n"
+            "  //////////////////           //////////////////(\n"
+            "                                                  \n"
+            "\e[0m"
         );
     }
 }
-
