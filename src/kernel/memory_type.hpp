a23301698f0b1af7bf31eafa7cc43a425721fb51
horizon <rekka728@gmail.com>
2023-12-24 12:48:48 +0900

    change
    change memory_type to C++-style from C-style

---
+++ src/kernel/memory_type.hpp
@@ -0,0 +1,26 @@
+#ifndef MEMORY_TYPE_H
+#define MEMORY_TYPE_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <common/types.hpp>
+
+namespace kernel
+{
+    struct memory_map_entry
+    {
+        common::physical_address start_physical_address;
+        common::word page_count;
+        bool is_free;
+        bool is_device;
+    };
+
+    struct memory_info
+    {
+        common::word memory_size;
+        uint16_t memory_map_count;
+        memory_map_entry *memory_map;
+    };
+}
+
+#endif
