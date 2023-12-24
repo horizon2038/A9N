ee1c75da56df4ddbede3154d516f2a7ba0db705e
horizon <horizon@horizon-mac-14.local>
2023-12-24 10:33:13 +0900

    clean
    kernel.cpp : fix headers include-directive

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -1,21 +1,27 @@
 #include "kernel.hpp"
-#include "interrupt_manager.hpp"
-#include "ipc_manager.hpp"
+
+#include <interrupt/interrupt_manager.hpp>
+#include <ipc/ipc_manager.hpp>
+#include <process/process.hpp>
+#include <process/process_manager.hpp>
+
 #include "memory_manager.hpp"
-#include "process.hpp"
-#include "process_manager.hpp"
 
 namespace kernel
 {
-    alignas(kernel::memory_manager) char kernel_object::memory_manager_buffer[memory_manager_size];
+    alignas(kernel::memory_manager
+    ) char kernel_object::memory_manager_buffer[memory_manager_size];
     kernel::memory_manager *kernel_object::memory_manager = nullptr;
 
-    alignas(kernel::interrupt_manager) char kernel_object::interrupt_manager_buffer[interrupt_manager_size];
+    alignas(kernel::interrupt_manager
+    ) char kernel_object::interrupt_manager_buffer[interrupt_manager_size];
     kernel::interrupt_manager *kernel_object::interrupt_manager = nullptr;
 
-    alignas(kernel::process_manager) char kernel_object::process_manager_buffer[process_manager_size];
+    alignas(kernel::process_manager
+    ) char kernel_object::process_manager_buffer[process_manager_size];
     kernel::process_manager *kernel_object::process_manager = nullptr;
 
-    alignas(kernel::ipc_manager) char kernel_object::ipc_manager_buffer[ipc_manager_size];
+    alignas(kernel::ipc_manager
+    ) char kernel_object::ipc_manager_buffer[ipc_manager_size];
     kernel::ipc_manager *kernel_object::ipc_manager = nullptr;
 }
