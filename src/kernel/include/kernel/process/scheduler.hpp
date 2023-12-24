c35c802db1d779018e22cf1e89e5255b002f9e8d
horizon <rekka728@gmail.com>
2023-12-24 16:12:28 +0900

    clean
    kernel/process/* : fix include-directive

--- src/kernel/include/kernel/process/scheduler.hpp
+++ src/kernel/include/kernel/process/scheduler.hpp
@@ -2,8 +2,8 @@
 #define SCHEDULER_HPP
 
 #include <stdint.h>
-#include <common/types.hpp>
-#include <process/process.hpp>
+#include <library/common/types.hpp>
+#include <kernel/process/process.hpp>
 
 namespace kernel
 {
@@ -17,22 +17,23 @@ namespace kernel
 
     class scheduler
     {
+      public:
+        scheduler(process *process_list);
+        ~scheduler();
 
-        public:
-            scheduler(process *process_list);
-            ~scheduler();
+        process *schedule_next_process();
+        process *schedule_next_process(
+            process *priority_groups[],
+            common::sword &priority
+        );
 
-            process *schedule_next_process();
-            process *schedule_next_process(process *priority_groups[], common::sword &priority);
+      private:
+        process *_process_list; // process array pointer<
+        process *_current_process;
+        common::word current_process_index;
 
-        private:
-            process *_process_list; // process array pointer<
-            process *_current_process;
-            common::word current_process_index;
-
-            void move_to_end(process *target_process,  process *&head_process);
-            common::sword update_highest_priority(process *priority_groups[]); 
-
+        void move_to_end(process *target_process, process *&head_process);
+        common::sword update_highest_priority(process *priority_groups[]);
     };
 }
 
