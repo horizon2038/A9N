cf220f2f0b03e510ad639c4f54e9e5aa03e38938
horizon <rekka728@gmail.com>
2023-12-25 13:39:22 +0900

    clean
    interrupt/* : fix include-directive

--- src/hal/x86_64/interrupt/interrupt.cpp
+++ src/hal/x86_64/interrupt/interrupt.cpp
@@ -1,9 +1,8 @@
-#include "interrupt.hpp"
+#include <hal/x86_64/interrupt/interrupt.hpp>
 
-#include "interrupt_descriptor.hpp"
+#include <hal/x86_64/interrupt/interrupt_descriptor.hpp>
 
-#include <library/logger.hpp>
-#include "../../kernel/kernel.hpp"
+#include <kernel/utility/logger.hpp>
 
 namespace hal::x86_64
 {
@@ -22,17 +21,21 @@ namespace hal::x86_64
 
         switch (irq_number)
         {
-            case 0:
+            case 0 :
                 interrupt_handler_table[irq_number]();
                 break;
 
-            default:
-                kernel::utility::logger::printk("%d exception %s : %llu\n", irq_number, exception_type, error_code);
+            default :
+                kernel::utility::logger::printk(
+                    "%d exception %s : %llu\n",
+                    irq_number,
+                    exception_type,
+                    error_code
+                );
         }
     }
 
-    interrupt::interrupt()
-        : _pic()
+    interrupt::interrupt() : _pic()
     {
     }
 
@@ -52,22 +55,29 @@ namespace hal::x86_64
     {
         for (uint16_t i = 0; i < 256; i++)
         {
-            register_idt_handler(i, (hal::interface::interrupt_handler)&interrupt_handlers[i]);
+            register_idt_handler(
+                i,
+                (hal::interface::interrupt_handler)&interrupt_handlers[i]
+            );
         }
     }
 
     void interrupt::load_idt()
     {
         constexpr uint16_t idt_size = sizeof(idt) - 1;
-        uint64_t *idt_address = (uint64_t*)idt;
+        uint64_t *idt_address = (uint64_t *)idt;
         _load_idt(idt_size, idt_address);
         disable_interrupt_all();
     }
 
-    void interrupt::register_idt_handler(common::word irq_number, hal::interface::interrupt_handler target_interrupt_handler)
+    void interrupt::register_idt_handler(
+        common::word irq_number,
+        hal::interface::interrupt_handler target_interrupt_handler
+    )
     {
         interrupt_descriptor_64 *idt_entry = &idt[irq_number];
-        uint64_t interrupt_handler_address = reinterpret_cast<uint64_t>(target_interrupt_handler);
+        uint64_t interrupt_handler_address
+            = reinterpret_cast<uint64_t>(target_interrupt_handler);
 
         idt_entry->offset_low = interrupt_handler_address & 0xffff;
         idt_entry->kernel_cs = 0x08; // kernel code segment
@@ -78,12 +88,20 @@ namespace hal::x86_64
         idt_entry->reserved = 0;
     };
 
-    void interrupt::register_handler(common::word irq_number, hal::interface::interrupt_handler target_interrupt_handler)
+    void interrupt::register_handler(
+        common::word irq_number,
+        hal::interface::interrupt_handler target_interrupt_handler
+    )
     {
         interrupt_handler_table[irq_number] = target_interrupt_handler;
-        uint64_t interrupt_handler_address = reinterpret_cast<uint64_t>(target_interrupt_handler);
+        uint64_t interrupt_handler_address
+            = reinterpret_cast<uint64_t>(target_interrupt_handler);
 
-        kernel::utility::logger::printk("hal_register_interrupt : %lu : 0x%016llx\n", irq_number, interrupt_handler_address);
+        kernel::utility::logger::printk(
+            "hal_register_interrupt : %lu : 0x%016llx\n",
+            irq_number,
+            interrupt_handler_address
+        );
     }
 
     void interrupt::enable_interrupt(common::word irq_number)
