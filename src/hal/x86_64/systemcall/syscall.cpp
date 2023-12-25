23eb9faf2666287a1d4d7ceb548f49e4c90a4786
horizon <rekka728@gmail.com>
2023-12-25 13:53:59 +0900

    clean
    syscall : fix include-directive

--- src/hal/x86_64/systemcall/syscall.cpp
+++ src/hal/x86_64/systemcall/syscall.cpp
@@ -1,12 +1,13 @@
-#include "syscall.hpp"
-#include "segment_configurator.hpp"
+#include <hal/x86_64/systemcall/syscall.hpp>
+
+#include <hal/x86_64/arch/segment_configurator.hpp>
 
 namespace hal::x86_64
 {
 
     extern "C" void _write_msr(uint32_t msr, uint64_t value);
     extern "C" uint64_t _read_msr(uint32_t msr);
-    
+
     void test_lstar()
     {
     }
@@ -17,14 +18,11 @@ namespace hal::x86_64
         _write_msr(MSR::SFMASK, 0x0002);
         _write_msr(MSR::LSTAR, reinterpret_cast<uint64_t>(test_lstar));
 
-        uint64_t star_value =
-        (
-            static_cast<uint64_t>(segment_selector::KERNEL_CS >> 32) | 
-            static_cast<uint64_t>(segment_selector::USER_CS_NULL << 48)
-        );
+        uint64_t star_value
+            = (static_cast<uint64_t>(segment_selector::KERNEL_CS >> 32)
+               | static_cast<uint64_t>(segment_selector::USER_CS_NULL << 48));
         _write_msr(MSR::STAR, star_value);
         _write_msr(MSR::CSTAR, 0);
-    
     }
 
     void syscall::handle_syscall()
