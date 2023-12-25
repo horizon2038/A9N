206dc2cbdda6089300afed2cd77a800ca862290d
horizon <rekka728@gmail.com>
2023-12-25 13:52:28 +0900

    clean
    process : fix include-directive

--- src/hal/x86_64/process/process_manager.cpp
+++ src/hal/x86_64/process/process_manager.cpp
@@ -1,18 +1,20 @@
-#include "process_manager.hpp"
-#include "common.hpp"
-#include "process.hpp"
+#include <hal/x86_64/process/process_manager.hpp>
 
-#include <library/string.hpp>
-#include <library/logger.hpp>
+#include <hal/x86_64/memory/paging.hpp>
 
-#include "paging.hpp"
+#include <kernel/process/process.hpp>
+#include <kernel/utility/logger.hpp>
 
-extern "C" void _switch_context(kernel::virtual_address *preview_stack_pointer, kernel::virtual_address *next_stack_pointer);
+#include <library/libc/string.hpp>
+
+extern "C" void _switch_context(
+    library::common::virtual_address *preview_stack_pointer,
+    library::common::virtual_address *next_stack_pointer
+);
 
 namespace hal::x86_64
 {
-    process_manager::process_manager()
-        : _segment_configurator()
+    process_manager::process_manager() : _segment_configurator()
     {
     }
 
@@ -20,17 +22,29 @@ namespace hal::x86_64
     {
     }
 
-    void process_manager::switch_context(kernel::process *preview_process, kernel::process *next_process)
+    void process_manager::switch_context(
+        kernel::process *preview_process,
+        kernel::process *next_process
+    )
     {
         _load_cr3(next_process->page_table);
         _segment_configurator.configure_rsp0(next_process->stack_pointer);
-        _switch_context(&preview_process->stack_pointer, &next_process->stack_pointer);
+        _switch_context(
+            &preview_process->stack_pointer,
+            &next_process->stack_pointer
+        );
     }
 
-    void process_manager::create_process(kernel::process *target_process, kernel::virtual_address entry_point_address)
+    void process_manager::create_process(
+        kernel::process *target_process,
+        common::virtual_address entry_point_address
+    )
     {
         std::memset(&target_process->stack, 0, kernel::STACK_SIZE_MAX);
-        kernel::virtual_address *target_stack_pointer = reinterpret_cast<kernel::virtual_address*>(&target_process->stack[kernel::STACK_SIZE_MAX]);
+        common::virtual_address *target_stack_pointer
+            = reinterpret_cast<common::virtual_address *>(
+                &target_process->stack[kernel::STACK_SIZE_MAX]
+            );
 
         /*
         init callee-saved register stacks.
@@ -53,10 +67,15 @@ namespace hal::x86_64
         *--target_stack_pointer = 0;
         // setup return address (program_counter) to stacks.
 
-        target_process->stack_pointer = reinterpret_cast<kernel::virtual_address>(target_stack_pointer);
+        target_process->stack_pointer
+            = reinterpret_cast<common::virtual_address>(target_stack_pointer);
 
-        kernel::utility::logger::printk("entry_point_address : %llx\n", entry_point_address);
-        // target_process->stack_pointer = convert_virtual_to_physical_address(target_process->stack_pointer);
+        kernel::utility::logger::printk(
+            "entry_point_address : %llx\n",
+            entry_point_address
+        );
+        // target_process->stack_pointer =
+        // convert_virtual_to_physical_address(target_process->stack_pointer);
     }
 
     void process_manager::delete_process(kernel::process *target_process)
