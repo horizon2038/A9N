c38ca196eca5780df85cf7cd4c12285142c7ab70
horizon <horizon@horizon-mac-14.local>
2023-12-25 13:34:32 +0900

    clean
    pit_timer : fix include-directive

--- src/hal/x86_64/include/hal/x86_64/time/pit_timer.hpp
+++ src/hal/x86_64/include/hal/x86_64/time/pit_timer.hpp
@@ -1,13 +1,12 @@
 #ifndef X86_64_PIT_TIMER_HPP
 #define X86_64_PIT_TIMER_HPP
 
-#include <interface/timer.hpp>
+#include <hal/interface/timer.hpp>
 
-#include <interface/port_io.hpp>
+#include <hal/interface/port_io.hpp>
+#include <hal/interface/serial.hpp>
 
-#include <interface/serial.hpp>
-
-#include "pic.hpp"
+#include <hal/x86_64/interrupt/pic.hpp>
 
 #include <stdint.h>
 
@@ -15,22 +14,22 @@ namespace hal::x86_64
 {
     class pit_timer final : public hal::interface::timer
     {
-        public:
-            pit_timer();
-            ~pit_timer();
+      public:
+        pit_timer();
+        ~pit_timer();
 
-            void init_timer() override;
-            void configure_timer(uint16_t hz) override;
-            void clock() override;
-            uint32_t get_tick() override;
+        void init_timer() override;
+        void configure_timer(uint16_t hz) override;
+        void clock() override;
+        common::word get_tick() override;
 
-            __attribute__((interrupt("IRQ"))) static void handle(void *data);
+        __attribute__((interrupt("IRQ"))) static void handle(void *data);
 
-            uint32_t ticks = 0;
+        uint32_t ticks = 0;
 
-        private:
-            static pit_timer *this_timer;
-            hal::x86_64::port_io _port_io;
+      private:
+        static pit_timer *this_timer;
+        hal::x86_64::port_io _port_io;
     };
 }
 
