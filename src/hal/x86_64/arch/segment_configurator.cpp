37185852e6557bf94096afd5849f70a8d35dae69
horizon <rekka728@gmail.com>
2023-12-25 13:05:14 +0900

    clean
    segment_configurator : fix include-directive

--- src/hal/x86_64/arch/segment_configurator.cpp
+++ src/hal/x86_64/arch/segment_configurator.cpp
@@ -1,9 +1,9 @@
-#include "segment_configurator.hpp"
+#include <hal/x86_64/arch/segment_configurator.hpp>
 #include <stdint.h>
 
-#include <library/logger.hpp>
-#include <library/string.hpp>
-#include <common.hpp>
+#include <kernel/utility/logger.hpp>
+#include <library/libc/string.hpp>
+#include <library/common/types.hpp>
 
 namespace hal::x86_64
 {
@@ -47,11 +47,10 @@ namespace hal::x86_64
     {
         uint64_t tss_address = reinterpret_cast<uint64_t>(&tss);
 
-        gdt.tss_low = 0x0000890000000000 |
-        sizeof(tss) |
-        ((tss_address & 0xffff) << 16) |
-        (((tss_address >> 16) & 0xff) << 32) |
-        (((tss_address >> 24) & 0xff) << 56);
+        gdt.tss_low = 0x0000890000000000 | sizeof(tss)
+                    | ((tss_address & 0xffff) << 16)
+                    | (((tss_address >> 16) & 0xff) << 32)
+                    | (((tss_address >> 24) & 0xff) << 56);
 
         gdt.tss_high = tss_address >> 32;
     }
@@ -59,11 +58,13 @@ namespace hal::x86_64
     void segment_configurator::load_gdt()
     {
         uint16_t gdt_size = sizeof(gdt) - 1;
-        uint64_t *gdt_address = (uint64_t*)&gdt;
+        uint64_t *gdt_address = (uint64_t *)&gdt;
         _load_gdt(gdt_size, gdt_address);
     }
 
-    void segment_configurator::load_segment_register(uint16_t code_segment_register)
+    void segment_configurator::load_segment_register(
+        uint16_t code_segment_register
+    )
     {
         _load_segment_register(code_segment_register);
     }
@@ -73,9 +74,10 @@ namespace hal::x86_64
         _load_task_register(segment_register);
     }
 
-    void segment_configurator::configure_rsp0(kernel::virtual_address kernel_stack_pointer)
+    void segment_configurator::configure_rsp0(
+        common::virtual_address kernel_stack_pointer
+    )
     {
         tss.rsp_0 = kernel_stack_pointer;
     }
 }
-
