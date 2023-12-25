4ede41d196da5c0d5d8e6c0b15263bc77f44e8ea
horizon <rekka728@gmail.com>
2023-12-25 13:33:56 +0900

    clean
    fix include-directive

--- src/hal/x86_64/include/hal/x86_64/process/process_manager.hpp
+++ src/hal/x86_64/include/hal/x86_64/process/process_manager.hpp
@@ -1,25 +1,31 @@
 #ifndef X86_64_CONTEXT_SWITCH_HPP
 #define X86_64_CONTEXT_SWITCH_HPP
 
-#include "common.hpp"
-#include <interface/process_manager.hpp>
+#include <library/common/types.hpp>
+#include <hal/interface/process_manager.hpp>
 
-#include "segment_configurator.hpp"
-#include "paging.hpp"
+#include <hal/x86_64/arch/segment_configurator.hpp>
+#include <hal/x86_64/memory/paging.hpp>
 
 namespace hal::x86_64
 {
     class process_manager final : public hal::interface::process_manager
     {
-        public:
-            process_manager();
-            ~process_manager();
-            void switch_context(kernel::process *preview_process, kernel::process *next_process) override;
-            void create_process(kernel::process *target_process, kernel::virtual_address entry_point_address) override;
-            void delete_process(kernel::process *target_process) override;
-        
-        private:
-            segment_configurator _segment_configurator;
+      public:
+        process_manager();
+        ~process_manager();
+        void switch_context(
+            kernel::process *preview_process,
+            kernel::process *next_process
+        ) override;
+        void create_process(
+            kernel::process *target_process,
+            common::virtual_address entry_point_address
+        ) override;
+        void delete_process(kernel::process *target_process) override;
+
+      private:
+        segment_configurator _segment_configurator;
     };
 }
 
