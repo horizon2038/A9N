ecf5fc2a3b1ebeed0d60cdaec30d6e2f0a1b844e
horizon <rekka728@gmail.com>
2023-12-25 13:22:54 +0900

    clean
    serial.hpp : fix include-directive

--- src/hal/x86_64/include/hal/x86_64/io/serial.hpp
+++ src/hal/x86_64/include/hal/x86_64/io/serial.hpp
@@ -1,29 +1,28 @@
 #ifndef X86_64_SERIAL_HPP
 #define X86_64_SERIAL_HPP
 
-#include <interface/serial.hpp>
+#include <hal/interface/serial.hpp>
 
+#include <hal/interface/port_io.hpp>
 #include <stdint.h>
-#include <interface/port_io.hpp>
 
 namespace hal::x86_64
 {
     class serial : public hal::interface::serial
     {
-        public:
-            serial(hal::interface::port_io &injected_port_io);
-            ~serial();
-            void init_serial(uint32_t baud_rate) override;
-            uint8_t read_serial() override;
-            void write_serial(char data) override;
-            void write_string_serial(char *out) override;
+      public:
+        serial(hal::interface::port_io &injected_port_io);
+        ~serial();
+        void init_serial(common::word baud_rate) override;
+        uint8_t read_serial() override;
+        void write_serial(char data) override;
+        void write_string_serial(char *out) override;
 
-        private:
-            hal::interface::port_io &_port_io;
-            int is_received();
-            int is_empty();
-            uint32_t strlen(const char *s);
-
+      private:
+        hal::interface::port_io &_port_io;
+        int is_received();
+        int is_empty();
+        uint32_t strlen(const char *s);
     };
 
 }
