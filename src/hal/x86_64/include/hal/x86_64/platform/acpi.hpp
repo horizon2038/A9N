1fc56c84a52140360065f78843b6af386165def1
horizon <rekka728@gmail.com>
2024-01-05 08:39:09 +0900

    [add] FADT structure
    add FADT structure and generic_address_structure to acpi.hpp

--- src/hal/x86_64/include/hal/x86_64/platform/acpi.hpp
+++ src/hal/x86_64/include/hal/x86_64/platform/acpi.hpp
@@ -14,7 +14,7 @@ namespace hal::x86_64
         uint8_t revision;
         uint32_t rsdt_address;
 
-        // ACPI V2 field
+        // ACPI v2 field
         uint32_t length;
         uint64_t xsdt_address;
         uint8_t extended_checksum;
@@ -38,6 +38,82 @@ namespace hal::x86_64
     {
         sdt_header header;
         uint64_t *other_sdt;
+    } __attribute__((packed));
+
+    struct generic_address_structure
+    {
+        uint8_t address_space;
+        uint8_t bit_width;
+        uint8_t bit_offset;
+        uint8_t access_size;
+        uint64_t address;
+    } __attribute__((packed));
+
+    struct fadt
+    {
+        sdt_header header;
+        uint32_t firmware_control;
+        uint32_t dsdt;
+
+        // ACPI v1 reserved.
+        uint8_t reserved;
+
+        uint8_t preferred_power_management_profile;
+        uint16_t sci_interrupt;
+        uint32_t smi_commandport;
+        uint8_t acpi_enable;
+        uint8_t acpi_disable;
+        uint8_t s4bios_request;
+        uint8_t pstate_control;
+        uint32_t pm1a_event_block;
+        uint32_t pm1b_event_block;
+        uint32_t pm1a_control_block;
+        uint32_t pm1b_control_block;
+        uint32_t pm2_control_block;
+        uint32_t pm_timer_block;
+        uint32_t gpe0_block;
+        uint32_t gpe1_block;
+        uint8_t pm1_event_length;
+        uint8_t pm1_control_length;
+        uint8_t pm2_control_length;
+        uint8_t pm_timer_length;
+        uint8_t gpe0_length;
+        uint8_t gpe1_length;
+        uint8_t gpe1_base;
+        uint8_t cstate_control;
+        uint16_t worst_c2_latency;
+        uint16_t worst_c3_latency;
+        uint16_t flush_size;
+        uint16_t flush_stride;
+        uint8_t duty_offset;
+        uint8_t duty_width;
+        uint8_t day_alarm;
+        uint8_t month_alarm;
+        uint8_t century;
+
+        // ACPI v1 reserved.
+        uint16_t boot_architecture_flags;
+
+        uint8_t reserved_2;
+        uint32_t flags;
+
+        generic_address_structure reset_register;
+
+        uint8_t reset_value;
+        uint8_t reserved_3[3];
+
+        // ACPI v2 field
+        uint64_t x_firmware_control;
+        uint64_t x_dsdt;
+
+        generic_address_structure x_pm1a_event_block;
+        generic_address_structure x_pm1b_event_block;
+        generic_address_structure x_pm1a_control_block;
+        generic_address_structure x_pm1_bcontrol_block;
+        generic_address_structure x_pm2_control_block;
+        generic_address_structure x_pm_timer_block;
+        generic_address_structure x_gpe0_block;
+        generic_address_structure x_gpe1_block;
     } __attribute__((packed));
 
     namespace ACPI_REGION
