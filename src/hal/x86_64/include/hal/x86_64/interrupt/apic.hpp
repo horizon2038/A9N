5e011f0992a6decba319eff35662b4bf80cae2f9
horizon <rekka728@gmail.com>
2024-01-06 18:40:14 +0900

    [add] apic source file
    add apic.cpp and apic.hpp

---
+++ src/hal/x86_64/include/hal/x86_64/interrupt/apic.hpp
@@ -0,0 +1,29 @@
+#ifndef HAL_x86_64_APIC_HPP
+#define HAL_x86_64_APIC_HPP
+
+#include <library/common/types.hpp>
+
+namespace hal::x86_64
+{
+    namespace LOCAL_APIC_ADDRESS
+    {
+        constexpr static uint32_t BASE_REGISTER = 0xfee00000;
+        constexpr static uint32_t ID_REGISTER = BASE_REGISTER + 0x20;
+        constexpr static uint32_t VERSION = BASE_REGISTER + 0x30;
+        constexpr static uint32_t TASK_PRIORITY_REGISTOR = BASE_REGISTER + 0x80;
+        constexpr static uint32_t ARBITRATION_PRIORITY_REGISTOR
+            = BASE_REGISTER + 0x90;
+        constexpr static uint32_t PROCESSOR_PRIORITY_REGISTOR
+            = BASE_REGISTER + 0xa0;
+        constexpr static uint32_t EOI = BASE_REGISTER + 0xb0;
+    }
+
+    namespace IO_APIC_ADDRESS
+    {
+        constexpr static uint32_t IO_REGISTER_SELECT = 0xfec00000;
+        constexpr static uint32_t IO_REGISTER_WINDOW = 0xfec00010;
+    }
+
+}
+
+#endif
