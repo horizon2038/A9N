2b7c8be10452fa58e61147e0752ea35e138f2c4e
horizon <rekka728@gmail.com>
2024-02-21 14:58:25 +0900

    [update] setup build enviroment for A9N
    setup EDK2 and qemu

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,29 @@
-#!/usr/bin/bash
+#!/bin/bash
 
+# common setup
+ARCH="x86_64"
+CHAINDIR=../chain
+SRCDIR=../src
+RUNDIR=../run
 
-apt-get update
-apt-get install -y \
-    bash \
-    clang \
-    clang++ \
-    llvm \
-    lld \
-    make
+CURRENT=`dirname $0`
+CURRENT=$(cd $(dirname $0);pwd)
+echo $CURRENT
+
+# setup EDK2
+pwd
+cd $CURRENT
+mkdir -p $CHAINDIR/$ARCH
+cd $CHAINDIR/$ARCH
+git clone https://github.com/tianocore/edk2.git
+cd edk2
+git submodule update --init
+source edksetup.sh
+make -C BaseTools/
+cd $CURRENT
+cp -f $SRCDIR/boot/target.txt $CHAINDIR/$ARCH/edk2/Conf/
+
+# setup qemu
+mkdir -p $RUNDIR/$ARCH/qemu
+cp /usr/share/OVMF/OVMF_CODE.fd $RUNDIR/$ARCH/qemu/
+cp /usr/share/OVMF/OVMF_VARS.fd $RUNDIR/$ARCH/qemu/
