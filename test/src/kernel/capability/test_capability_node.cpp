983739dbb63b45c2464d6e18ab7f5e1eeec3a79b
horizon <rekka728@gmail.com>
2024-02-22 14:50:51 +0900

    [add] capability_slot

---
+++ test/src/kernel/capability/test_capability_node.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+#include <stdio.h>
+
+class capability_node_test : public ::testing::Test
+{
+  public:
+    void SetUp() override
+    {
+    }
+
+    void TearDown() override
+    {
+    }
+
+  private:
+};
