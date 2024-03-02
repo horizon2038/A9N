a125da2bcb4c1fb33e7ec0b55a1b9fbb0832c23a
horizon <rekka728@gmail.com>
2024-03-02 23:15:05 +0900

    [add] convert_argment constant

--- src/library/include/library/capability/generic_operation.hpp
+++ src/library/include/library/capability/generic_operation.hpp
@@ -11,6 +11,26 @@ namespace library::capability
     {
         CONVERT,
     };
+
+    namespace convert_argument
+    {
+        // 0 : descriptor (this) [reserved]
+        // 1 : descriptor_depth  [reserved]
+        // 2 : operation_type
+        // 3 : type
+        // 4 : size
+        // 5 : count
+        // 6 : target node descriptor
+        // 7 : target node depth
+        // 8 : target node index
+
+        constexpr inline common::word CAPABILITY_TYPE = 3;
+        constexpr inline common::word CAPABILITY_SIZE = 4;
+        constexpr inline common::word CAPABILITY_COUNT = 5;
+        constexpr inline common::word ROOT_DESCRIPTOR = 6;
+        constexpr inline common::word ROOT_DEPTH = 7;
+        constexpr inline common::word SLOT_INDEX = 8;
+    };
 }
 
 #endif
