4add25be25fa70bc21b0403918a7ada20ad961db
horizon <rekka728@gmail.com>
2024-03-07 15:33:32 +0900

    [add] rudimentary option type

---
+++ src/library/include/library/common/option.hpp
@@ -0,0 +1,97 @@
+#ifndef LIBH5N_OPTION_HPP
+#define LIBH5N_OPTION_HPP
+
+#include <library/libcpp/utility.hpp>
+
+namespace library::common
+{
+    template<typename T>
+    class option
+    {
+      public:
+        option() noexcept
+        {
+        }
+
+        explicit option(T value, bool has_value = false) noexcept
+            : value_(std::move(value))
+            , has_value_(has_value)
+        {
+        }
+
+        option(const option &other) noexcept
+            : value_(other.value_)
+            , has_value_(other.has_value_)
+        {
+        }
+
+        option &operator=(const option &other) noexcept
+        {
+            value_ = other.value_;
+            has_value_ = other.has_value_;
+            return *this;
+        }
+
+        option(option &&other) noexcept
+            : value_(std::move(other.value_))
+            , has_value_(other.has_value_)
+        {
+        }
+
+        option &operator=(option &&other) noexcept
+        {
+            value_ = std::move(other.value_);
+            has_value_ = other.has_value_;
+            return *this;
+        }
+
+        bool is_none() const noexcept
+        {
+            return !has_value_;
+        }
+
+        bool has_value() const noexcept
+        {
+            return has_value_;
+        }
+
+        T &unwrap() &
+        {
+            if (!has_value_)
+            {
+            }
+            return value_;
+        }
+
+        const T &unwrap() const &
+        {
+            if (!has_value_)
+            {
+            }
+            return value_;
+        }
+
+        T unwrap_or(T default_value) const &
+        {
+            return has_value_ ? value_ : default_value;
+        }
+
+      private:
+        bool has_value_;
+        T value_;
+    };
+
+    template<typename T>
+    option<T> some(T value)
+    {
+        return option<T>(std::move(value), true);
+    }
+
+    template<typename T>
+    option<T> none()
+    {
+        return option<T>();
+    }
+}
+
+#endif
