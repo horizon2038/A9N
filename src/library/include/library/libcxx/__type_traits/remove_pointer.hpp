e5d6d8280411de7aa5bc1008e2be8179e841edc8
horizon <rekka728@gmail.com>
2024-03-08 21:58:15 +0900

    [add] type_traits : remove_pointer<T>

---
+++ src/library/include/library/libcxx/__type_traits/remove_pointer.hpp
@@ -0,0 +1,40 @@
+#ifndef LIBCXX_REMOVE_POINTER_HPP
+#define LIBCXX_REMOVE_POINTER_HPP
+
+namespace library::std
+{
+    template<typename T>
+    struct remove_pointer
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_pointer<T *>
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_pointer<const T *>
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_pointer<volatile T *>
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    struct remove_pointer<const volatile T *>
+    {
+        using type = T;
+    };
+
+    template<typename T>
+    using remove_pointer_t = typename remove_pointer<T>::type;
+}
+
+#endif
