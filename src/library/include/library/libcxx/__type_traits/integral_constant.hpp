822758a9e31b421d02e874f1fde938d0cc0e0a3d
horizon <rekka728@gmail.com>
2024-03-08 22:39:25 +0900

    [add] type_traits : constant

---
+++ src/library/include/library/libcxx/__type_traits/integral_constant.hpp
@@ -0,0 +1,26 @@
+#ifndef LIBCXX_INTEGRAL_CONSTANT_HPP
+#define LIBCXX_INTEGRAL_CONSTANT_HPP
+
+namespace library::std
+{
+    template<typename T, T v>
+    struct integral_constant
+    {
+        static constexpr T value = v;
+
+        using value_type = T;
+        using type = integral_constant<T, v>;
+
+        constexpr operator value_type() noexcept
+        {
+            return value;
+        }
+
+        constexpr value_type operator()() const noexcept
+        {
+            return value;
+        }
+    };
+}
+
+#endif
