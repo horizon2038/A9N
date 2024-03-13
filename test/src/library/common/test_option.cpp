8bee5876f5a1e48bbe9b05910832360473cf9ec5
horizon <rekka728@gmail.com>
2024-03-13 18:23:44 +0900

    [add] test option<T>

--- test/src/library/common/test_option.cpp
+++ test/src/library/common/test_option.cpp
@@ -11,18 +11,28 @@ using word = library::common::word;
 
 TEST(option_test, option_has_value_test)
 {
-    auto opt = option<word>::some(word { 0x1 });
+    auto opt = library::common::make_option_some(0x40);
     ASSERT_EQ(true, opt.has_value());
 }
 
 TEST(option_test, option_unwrap_test)
 {
-    auto opt = option<word>::some(word { 0xdeadbeaf });
+    auto opt = library::common::make_option_some(word { 0xdeadbeaf });
     ASSERT_EQ(0xdeadbeaf, opt.unwrap());
 }
 
 TEST(option_test, option_none_test)
 {
-    auto opt = option<word>::none();
+    option<word> opt = library::common::make_option_none();
     ASSERT_EQ(false, opt.has_value());
+}
+
+TEST(option_test, option_ptr_test)
+{
+    word base_value = 400;
+    word *base_value_address = &base_value;
+    option<word *> opt = library::common::make_option_some(base_value_address);
+
+    auto addr = opt.unwrap();
+    ASSERT_EQ(400, *addr);
 }
