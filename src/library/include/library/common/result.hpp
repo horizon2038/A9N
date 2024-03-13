9154edbd616d9dfa8b0a78357167373b1ec17e13
horizon <rekka728@gmail.com>
2024-03-13 12:20:44 +0900

    [update] option<T> : infer type when called none()

--- src/library/include/library/common/result.hpp
+++ src/library/include/library/common/result.hpp
@@ -2,90 +2,160 @@
 #define LIBRARY_RESULT_HPP
 
 #include <library/libcxx/utility>
+#include <library/libcxx/type_traits>
 
 namespace library::common
 {
-    enum class result_state
+    template<typename, typename>
+    class result;
+
+    template<typename T, int x>
+    class GenericWrapper;
+
+    template<typename T>
+    using error_type = GenericWrapper<T, 0>;
+
+    template<typename T>
+    using value_type = GenericWrapper<T, 1>;
+
+    template<typename T, int x>
+    class GenericWrapper
     {
-        VALUE_TYPE,
-        ERROR_TYPE
+        T v;
+        GenericWrapper(T &&a) : v(library::std::forward<T>(a)) {};
+        GenericWrapper(const T &a) : v(a) {};
+        GenericWrapper(const GenericWrapper &) = default;
+        GenericWrapper(GenericWrapper &&) = default;
+
+      public:
+        ~GenericWrapper() = default;
+
+        template<typename, typename>
+        friend class result;
+
+        template<typename U>
+        friend auto error(U &&error)
+            -> error_type<library::std::remove_cvref_t<U>>;
+
+        template<typename U>
+        friend auto value(U &&value)
+            -> GenericWrapper<library::std::remove_cvref_t<U>, 1>;
     };
 
-    template<typename T, typename E>
+    template<typename T>
+    auto error(T &&error) -> error_type<library::std::remove_cvref_t<T>>
+    {
+        return { library::std::forward<T>(error) };
+    }
+
+    template<typename T>
+    auto value(T &&value) -> value_type<library::std::remove_cvref_t<T>>
+    {
+        return { library::std::forward<T>(value) };
+    }
+
+    template<typename T, typename ErrorT = int>
     class result
     {
-      public:
-        result() noexcept : state_(result_state::VALUE_TYPE), value_()
-        {
-        }
-        explicit result(T value) noexcept
-            : state_(result_state::VALUE_TYPE)
-            , value_(library::std::move(value))
-        {
-        }
-        explicit result(const E &error) noexcept
-            : state_(result_state::ERROR_TYPE)
-            , error_(error)
-        {
-        }
+        using Type = library::std::remove_cvref_t<T>;
+        using Error = library::std::remove_cvref_t<ErrorT>;
 
-        bool has_value() const noexcept
-        {
-            return state_ == result_state::VALUE_TYPE;
-        }
-        bool is_error() const noexcept
-        {
-            return state_ == result_state::ERROR_TYPE;
-        }
-
-        const T &value() const &
-        {
-            if (state_ != result_state::VALUE_TYPE)
-            {
-            }
-            return value_;
-        }
-
-        T &value() &
-        {
-            if (state_ != result_state::VALUE_TYPE)
-            {
-            }
-            return value_;
-        }
-
-        const E &error_value() const &
-        {
-            if (state_ != result_state::ERROR_TYPE)
-            {
-            }
-            return error_;
-        }
-
-        E &error_value() &
-        {
-            if (state_ != result_state::ERROR_TYPE)
-            {
-            }
-            return error_;
-        }
-
-        static result<T, E> ok(T value) noexcept
-        {
-            return result<T, E>(library::std::move(value));
-        }
-        static result<T, E> error(const E &error) noexcept
-        {
-            return result<T, E>(error);
-        }
-
-      private:
-        result_state state_;
         union
         {
-            T value_;
-            E error_;
-        };
+            Type value;
+            Error error;
+        } m_stored;
+        bool m_is_error = false;
+
+      public:
+        template<typename U>
+        requires(library::std::is_same_v<
+                 library::std::remove_cvref_t<
+                     library::std::remove_pointer_t<U>>,
+                 library::std::remove_cvref_t<
+                     library::std::remove_pointer_t<T>>>)
+            result(value_type<U> &&value)
+        {
+            new (&m_stored.value) T(library::std::move(value.v));
+        }
+
+        template<typename U>
+        requires(library::std::is_same_v<
+                 library::std::remove_cvref_t<
+                     library::std::remove_pointer_t<U>>,
+                 library::std::remove_cvref_t<
+                     library::std::remove_pointer_t<ErrorT>>>)
+            result(error_type<U> &&error)
+        {
+            m_is_error = true;
+            new (&m_stored.error) ErrorT(library::std::move(error.v));
+        }
+
+        ~result()
+        {
+            if (is_error())
+                m_stored.error.~ErrorT();
+            else
+                m_stored.value.~T();
+        }
+
+        result(const result &other)
+        {
+            if (other.is_error())
+            {
+                m_is_error = true;
+                m_stored.error = other.get_error();
+            }
+            else
+            {
+                m_stored.value = other.get_value();
+            }
+        }
+
+        result(result &&other)
+        {
+            if (other.is_error())
+            {
+                m_is_error = true;
+                m_stored.error = library::std::move(other.get_error());
+            }
+            else
+            {
+                m_stored.value = library::std::move(other.get_value());
+            }
+        }
+
+        bool is_error() const
+        {
+            return m_is_error;
+        }
+
+        bool is_value() const
+        {
+            return !is_error();
+        }
+
+        T &get_value()
+        {
+            const auto &as_const = *this;
+            return const_cast<T &>(as_const.get_value());
+        }
+
+        const T &get_value() const
+        {
+            return m_stored.value;
+        }
+
+        ErrorT &get_error()
+        {
+            const auto &as_const = *this;
+            return const_cast<ErrorT &>(as_const.get_error());
+        }
+
+        const ErrorT &get_error() const
+        {
+            return m_stored.error;
+        }
     };
 }
 
