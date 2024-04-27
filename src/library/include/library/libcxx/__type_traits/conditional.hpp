511a688250f90fe9c96579573deef9a39ed1bcc4
horizon <rekka728@gmail.com>
2024-04-27 20:32:45 +0900

    [add] library::std::conditional for type_traits

---
+++ src/library/include/library/libcxx/__type_traits/conditional.hpp
@@ -0,0 +1,17 @@
+namespace library::std
+{
+    template<bool B, typename T, typename F>
+    struct conditional
+    {
+        using type = T;
+    };
+
+    template<typename T, typename F>
+    struct conditional<false, T, F>
+    {
+        using type = F;
+    };
+
+    template<bool B, typename T, typename F>
+    using conditional_t = conditional<B, T, F>::type;
+}
