d295c2d7078ac5906af0c7a10122aa1c95201587
horizon <rekka728@gmail.com>
2024-06-22 09:17:54 +0900

    [add] test of library::std::function

---
+++ test/src/library/libcxx/test_function.cpp
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+
+#include <library/libcxx/functional>
+
+TEST(function_test, invoke_lambda_test)
+{
+    library::std::function<int(int, int)> func(
+        [](int a, int b)
+        {
+            return a + b;
+        }
+    );
+    ASSERT_EQ(func(2, 3), 5);
+}
+
+struct multiplier
+{
+    int operator()(int a, int b) const
+    {
+        return a * b;
+    }
+};
+
+TEST(function_test, invoke_function_object_test)
+{
+    library::std::function<int(int, int)> func(multiplier {});
+    ASSERT_EQ(func(4, 5), 20);
+}
+
+int subtract(int a, int b)
+{
+    return a - b;
+}
+
+TEST(function_test, invoke_free_function_test)
+{
+    library::std::function<int(int, int)> func(subtract);
+    ASSERT_EQ(func(9, 4), 5);
+}
