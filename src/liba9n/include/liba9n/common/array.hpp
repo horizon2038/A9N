422a82461222bb499fdf464a88caeedc79902758
horizon <rekka728@gmail.com>
2024-06-29 19:46:37 +0900

    [add] safe_array<T, auto Size>

--- src/liba9n/include/liba9n/common/array.hpp
+++ src/liba9n/include/liba9n/common/array.hpp
@@ -3,6 +3,8 @@
 
 #include <kernel/types.hpp>
 
+#include <liba9n/option/option.hpp>
+
 namespace liba9n::common
 {
     template<typename T, a9n::word size>
@@ -37,6 +39,44 @@ namespace liba9n::common
 
       private:
         T elements[size];
+    };
+
+    template<typename T, auto Size>
+    class safe_array
+    {
+        using size_type = decltype(Size);
+
+      private:
+        T elements[Size];
+
+      public:
+        liba9n::option<T> get_element(size_type index)
+        {
+            if (index >= Size) [[unlikely]]
+            {
+                return {};
+            }
+
+            return elements[index];
+        }
+
+        void set_element(size_type index, const T &value)
+        {
+            if (index >= Size) [[unlikely]]
+            {
+                return;
+            }
+
+            elements[index] = value;
+        }
+
+        void fill(const T &data)
+        {
+            for (size_type i = 0; i < Size; i++)
+            {
+                elements[i] = data;
+            }
+        }
     };
 }
 
