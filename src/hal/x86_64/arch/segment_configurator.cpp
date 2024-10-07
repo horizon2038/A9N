4fff95f01efa40b05c08bf8a6c750d891afcb070
horizon <rekka728@gmail.com>
2024-10-07 13:50:25 +0900

    [clean] segment_configurator

--- src/hal/x86_64/arch/segment_configurator.cpp
+++ src/hal/x86_64/arch/segment_configurator.cpp
@@ -5,14 +5,13 @@
 #include <kernel/utility/logger.hpp>
 #include <liba9n/libc/string.hpp>
 
+#include <hal/x86_64/arch/cpu.hpp>
+
 namespace a9n::hal::x86_64
 {
-    extern "C" void _load_gdt(uint16_t size, uint64_t *offset);
+    extern "C" void _load_gdt(uint16_t size, uint64_t offset);
     extern "C" void _load_segment_register(uint16_t code_segment_register);
     extern "C" void _load_task_register(uint16_t segment_register);
-
-    global_descriptor_table segment_configurator::gdt = {};
-    task_state_segment      segment_configurator::tss = {};
 
     segment_configurator::segment_configurator()
     {
@@ -31,34 +30,57 @@ namespace a9n::hal::x86_64
         load_gdt();
         load_segment_register(segment_selector::KERNEL_CS);
         load_task_register(segment_selector::KERNEL_TSS);
+
+        // TODO: configure per cpu
+        uint8_t *kernel_stack_top = reinterpret_cast<uint8_t *>(
+            reinterpret_cast<uint8_t *>(kernel_stacks[0].data()) + 4096
+        );
+        configure_rsp0(reinterpret_cast<a9n::virtual_address>(kernel_stack_top));
+
+        a9n::kernel::utility::logger::printk(
+            "kernel_stack address : 0x%016llx\n",
+            reinterpret_cast<a9n::virtual_address>(kernel_stack_top)
+        );
     }
 
     void segment_configurator::configure_gdt()
     {
-        gdt.null                 = 0;
-        gdt.kernel_code_segment  = 0x00af9a000000ffff;
-        gdt.kernel_data_segment  = 0x00cf93000000ffff;
+        gdt.kernel_code_segment  = segment_descriptor::KERNEL_CODE;
+        gdt.kernel_data_segment  = segment_descriptor::KERNEL_DATA;
         gdt.user_code_segment_32 = 0;
-        gdt.user_code_segment    = 0x00af9a000000ffff;
-        gdt.user_data_sgment     = 0x00cff2000000ffff;
+        gdt.user_code_segment    = segment_descriptor::USER_CODE;
+        gdt.user_data_segment    = segment_descriptor::USER_DATA;
     }
 
     void segment_configurator::configure_tss()
     {
+        // +-------+-----------------+
+        // | base  | tss address     |
+        // +-------+-----------------+
+        // | limit | sizeof(tss)     |
+        // +-------+-----------------+
+        // | flag  | 0x4 (size bits) |
+        // +-------+-----------------+
+
         uint64_t tss_address = reinterpret_cast<uint64_t>(&tss);
 
-        gdt.tss_low
-            = 0x0000890000000000 | sizeof(tss) | ((tss_address & 0xffff) << 16)
-            | (((tss_address >> 16) & 0xff) << 32)
-            | (((tss_address >> 24) & 0xff) << 56);
-
+        gdt.tss_low          = create_segment_descriptor(
+            (tss_address & 0xFFFFFFFF),
+            sizeof(tss),
+            create_system_segment_access_field(
+                true,
+                false,
+                system_segment_type::TSS_AVAILABLE_64
+            ),
+            0x4
+        );
         gdt.tss_high = tss_address >> 32;
     }
 
     void segment_configurator::load_gdt()
     {
-        uint16_t  gdt_size    = sizeof(gdt) - 1;
-        uint64_t *gdt_address = (uint64_t *)&gdt;
+        uint16_t gdt_size    = sizeof(global_descriptor_table) - 1;
+        uint64_t gdt_address = reinterpret_cast<uint64_t>(&gdt);
         _load_gdt(gdt_size, gdt_address);
     }
 
@@ -76,5 +98,6 @@ namespace a9n::hal::x86_64
     )
     {
         tss.rsp_0 = kernel_stack_pointer;
+        tss.ist_1 = kernel_stack_pointer;
     }
 }
--- src/hal/x86_64/include/hal/x86_64/arch/segment_configurator.hpp
+++ src/hal/x86_64/include/hal/x86_64/arch/segment_configurator.hpp
@@ -42,7 +42,7 @@ namespace a9n::hal::x86_64
         uint64_t kernel_data_segment;
         uint64_t user_code_segment_32; // unused
         uint64_t user_code_segment;
-        uint64_t user_data_sgment;
+        uint64_t user_data_segment;
         uint64_t tss_low;
         uint64_t tss_high;
 
@@ -66,9 +66,129 @@ namespace a9n::hal::x86_64
         void load_task_register(uint16_t segment_register);
 
         // it needs to be rewritten because it does not take into account mp
-        static task_state_segment      tss;
-        static global_descriptor_table gdt;
     };
+
+    inline constexpr uint64_t create_segment_descriptor(
+        uint32_t base,
+        uint32_t limit,
+        uint8_t  access,
+        uint8_t  flags
+    )
+    {
+        // +------------+-------+-------+-------------+-----------+
+        // |   63-56    | 55-52 | 51-48 |    47-40    |   39-32   |
+        // +------------+-------+-------+-------------+-----------+
+        // | base(high) | flags | limit | access byte | base(mid) |
+        // +------------+-------+-------+-------------+-----------+
+        // |           31-16            |          15-0           |
+        // +----------------------------+-------------------------+
+        // | base(low)                  | limit                   |
+        // +----------------------------+-------------------------+
+
+        uint64_t descriptor {};
+
+        descriptor |= static_cast<uint64_t>((base >> 24) & 0xFF) << 56;
+        descriptor |= static_cast<uint64_t>(flags & 0xF) << 52;
+        descriptor |= static_cast<uint64_t>((limit >> 16) & 0xF) << 48;
+        descriptor |= static_cast<uint64_t>(access) << 40;
+        descriptor |= static_cast<uint64_t>((base >> 16) & 0xFF) << 32;
+        descriptor |= static_cast<uint64_t>(base & 0xFFFF) << 16;
+        descriptor |= static_cast<uint64_t>(limit & 0xFFFF);
+
+        return descriptor;
+    }
+
+    inline constexpr uint8_t create_segment_access_field(
+        bool is_present,
+        bool is_user,
+        bool is_system_segment,
+        bool is_executable,
+        bool is_comforming,
+        bool is_readable_writable,
+        bool is_accessed = true
+    )
+    {
+        // +---------+-----+---+---+----+----+---+
+        // |    7    | 6-5 | 4 | 3 | 2  | 1  | 0 |
+        // +---------+-----+---+---+----+----+---+
+        // | present | dpl | s | e | dc | rw | a |
+        // +---------+-----+---+---+----+----+---+
+
+        uint8_t descriptor {};
+
+        descriptor |= static_cast<uint8_t>(is_present << 7);
+        descriptor |= static_cast<uint8_t>((is_user ? 3 : 0) << 5);
+        descriptor |= static_cast<uint8_t>(!is_system_segment << 4);
+        descriptor |= static_cast<uint8_t>(is_executable << 3);
+        descriptor |= static_cast<uint8_t>(is_comforming << 2);
+        descriptor |= static_cast<uint8_t>(is_readable_writable << 1);
+        descriptor |= static_cast<uint8_t>(is_accessed << 0);
+
+        return descriptor;
+    }
+
+    namespace system_segment_type
+    {
+        inline constexpr uint8_t LDT              = 0x2;
+        inline constexpr uint8_t TSS_AVAILABLE_64 = 0x9;
+        inline constexpr uint8_t TSS_WAIT_64      = 0xB;
+    };
+
+    inline constexpr uint8_t
+        create_system_segment_access_field(bool is_present, bool is_user, uint8_t type)
+    {
+        uint8_t descriptor {};
+
+        descriptor |= static_cast<uint8_t>(is_present << 7);
+        descriptor |= static_cast<uint8_t>((is_user ? 3 : 0) << 5);
+        descriptor |= static_cast<uint8_t>(type & 0xF);
+
+        return descriptor;
+    }
+
+    namespace segment_descriptor
+    {
+        // in long mode, the base and limit are ignored.
+        namespace detail
+        {
+            inline constexpr uint32_t BASE  = 0;
+            inline constexpr uint32_t LIMIT = 0xFFFFF;
+            inline constexpr uint8_t  CODE  = 0xA; // limit 4kib, long mode
+            inline constexpr uint8_t  DATA  = 0xA; // limit 4kib, long mode
+        }
+
+        inline constexpr uint64_t KERNEL_CODE = create_segment_descriptor(
+            detail::BASE,
+            detail::LIMIT,
+            create_segment_access_field(true, false, false, true, false, true),
+            detail::CODE
+        );
+
+        inline constexpr uint64_t KERNEL_DATA = create_segment_descriptor(
+            detail::BASE,
+            detail::LIMIT,
+            create_segment_access_field(true, false, false, false, false, true),
+            detail::DATA
+        );
+
+        inline constexpr uint64_t USER_CODE = create_segment_descriptor(
+            detail::BASE,
+            detail::LIMIT,
+            create_segment_access_field(true, true, false, true, false, true),
+            detail::CODE
+        );
+
+        inline constexpr uint64_t USER_DATA = create_segment_descriptor(
+            detail::BASE,
+            detail::LIMIT,
+            create_segment_access_field(true, true, false, false, false, true),
+            detail::DATA
+        );
+    }
+
+    // TODO: move to cpu_local_variable
+    alignas(8) inline task_state_segment tss;
+    alignas(8) inline global_descriptor_table gdt;
 
 }
 #endif
