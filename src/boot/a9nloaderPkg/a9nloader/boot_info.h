643fb5044912ea17335863cbabef7c138f189f5d
horizon <rekka728@gmail.com>
2024-10-18 23:22:07 +0900

    [add] a9nloader : init server info

--- src/boot/a9nloaderPkg/a9nloader/boot_info.h
+++ src/boot/a9nloaderPkg/a9nloader/boot_info.h
@@ -4,12 +4,14 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "init_image_info.h"
 #include "memory_type.h"
 
 typedef struct
 {
-    memory_info boot_memory_info;
-    uintmax_t   arch_info[8];
+    memory_info     boot_memory_info;
+    init_image_info boot_init_image_info;
+    uintmax_t       arch_info[8];
 } boot_info;
 
 #endif
