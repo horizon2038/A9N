a9c33826b516d2c5ddb8b8db24e5f32881884842
horizon <rekka728@gmail.com>
2024-11-01 23:29:33 +0900

    [update] liba9n::not_null<T>

--- src/liba9n/include/liba9n/common/not_null.hpp
+++ src/liba9n/include/liba9n/common/not_null.hpp
@@ -1,6 +1,7 @@
 #ifndef LIBA9N_NOT_NULL_HPP
 #define LIBA9N_NOT_NULL_HPP
 
+#include "liba9n/libcxx/__type_traits/is_convertible.hpp"
 #include <liba9n/libcxx/type_traits>
 #include <liba9n/libcxx/utility>
 
@@ -9,47 +10,83 @@ namespace liba9n
     template<typename T>
     class not_null
     {
+        // type definition
         using TElement   = liba9n::std::remove_reference_t<liba9n::std::remove_pointer_t<T>>;
         using TReference = TElement &;
         using TPointer   = TElement *;
 
+        // friend definition
+        template<typename U>
+        friend class not_null;
+
       public:
-        template<typename U>
+        template<typename U = T>
             requires(liba9n::std::is_convertible_v<
-                     TPointer,
-                     liba9n::std::remove_reference_t<liba9n::std::remove_pointer_t<U>> *>)
-        not_null(U &&reference)
-            : pointer(liba9n::std::addressof(liba9n::std::forward<U>(reference)))
+                     liba9n::std::remove_reference_t<liba9n::std::remove_pointer_t<U>> *,
+                     TPointer>)
+        constexpr not_null(const U &reference) : pointer(liba9n::std::addressof(reference))
+        {
+        }
+
+        constexpr not_null(const not_null &other) = default;
+        constexpr not_null(not_null &&other)      = default;
+
+        template<typename U>
+            requires(liba9n::std::is_convertible_v<typename not_null<U>::TPointer, TPointer>)
+        constexpr not_null(const not_null<U> &other) : pointer(liba9n::std::addressof(other.get()))
         {
         }
 
         template<typename U>
-            requires(liba9n::std::is_convertible_v<
-                     TPointer,
-                     liba9n::std::remove_reference_t<liba9n::std::remove_pointer_t<U>> *>)
-        not_null(const U &reference) : pointer(liba9n::std::addressof(reference))
+            requires(liba9n::std::is_convertible_v<typename not_null<U>::TPointer, TPointer>)
+        constexpr not_null(not_null<U> &&other) : pointer(liba9n::std::addressof(other.get()))
         {
         }
 
-        not_null(const not_null &other) = default;
-        not_null(not_null &&other)      = default;
+        // operator section
+        template<typename U = T>
+            requires(liba9n::std::is_convertible_v<
+                     liba9n::std::remove_reference_t<liba9n::std::remove_pointer_t<U>> *,
+                     TPointer>)
+        constexpr not_null &operator=(const U &reference)
+        {
+            pointer = liba9n::std::addressof(reference);
+        }
 
-        TReference get() const noexcept
+        constexpr not_null &operator=(const not_null &other) = default;
+        constexpr not_null &operator=(not_null &&other)      = default;
+
+        template<typename U>
+            requires(liba9n::std::is_convertible_v<typename not_null<U>::TPointer, TPointer>)
+        constexpr not_null &operator=(const not_null<U> &other)
+        {
+            pointer = liba9n::std::addressof(other.get());
+        }
+
+        template<typename U>
+            requires(liba9n::std::is_convertible_v<typename not_null<U>::TPointer, TPointer>)
+        constexpr not_null &operator=(not_null<U> &&other)
+        {
+            pointer = liba9n::std::addressof(other.get());
+        }
+
+        constexpr TReference operator*() const noexcept
         {
             return *pointer;
         }
 
-        TReference operator*() const noexcept
-        {
-            return *pointer;
-        }
-
-        TPointer operator->() const noexcept
+        constexpr TPointer operator->() const noexcept
         {
             return pointer;
         }
 
-        explicit operator bool() const noexcept = delete;
+        constexpr explicit operator bool() const noexcept = delete;
+
+        // member methods
+        constexpr TReference get() const noexcept
+        {
+            return *pointer;
+        }
 
       private:
         TPointer pointer;
