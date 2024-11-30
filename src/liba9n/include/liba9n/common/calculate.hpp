6fd874c0e0fefd243b66da34cc0758f990bb8b9f
horizon <rekka728@gmail.com>
2024-11-30 14:34:02 +0900

    [update] calculate_radix_*

--- src/liba9n/include/liba9n/common/calculate.hpp
+++ src/liba9n/include/liba9n/common/calculate.hpp
@@ -5,13 +5,40 @@
 
 namespace liba9n
 {
-    inline a9n::word round_up_power_of_2(a9n::word value)
+    inline constexpr a9n::word round_up_power_of_2(a9n::word value)
     {
-        a9n::word power = 1;
-        while (power < value)
+        [[unlikely]] if (!value)
         {
-            power <<= 1;
+            return 1;
         }
+
+        if ((value & (value - 1)) == 0)
+        {
+            return value;
+        }
+
+        a9n::word leading_zero   = __builtin_clzll(value);
+        a9n::word highest_bit    = a9n::WORD_BITS - 1 - leading_zero;
+
+        a9n::word power          = 1;
+        power                  <<= (highest_bit + 1);
+
+        return power;
+    }
+
+    inline constexpr a9n::word round_down_power_of_2(a9n::word value)
+    {
+        [[unlikely]] if (!value)
+        {
+            return 0;
+        }
+
+        a9n::word leading_zero   = __builtin_clzll(value);
+        a9n::word highest_bit    = a9n::WORD_BITS - 1 - leading_zero;
+
+        a9n::word power          = 1;
+        power                  <<= highest_bit;
+
         return power;
     }
 
@@ -32,10 +59,50 @@ namespace liba9n
         return value;
     }
 
-    inline a9n::word aligne_up_power_of_2(a9n::word value, a9n::word base)
+    inline a9n::word align_up_power_of_2(a9n::word value, a9n::word base)
     {
         value += (base - 1);
         return align_down_power_of_2(value, base);
+    }
+
+    inline constexpr a9n::word calculate_radix(a9n::word power_of_2)
+    {
+        [[unlikely]] if (!power_of_2)
+        {
+            return 0;
+        }
+
+        a9n::word radix = 0;
+        while (power_of_2 >>= 1)
+        {
+            radix++;
+        }
+
+        return radix;
+    }
+
+    inline constexpr a9n::word calculate_radix_ceil(a9n::word value)
+    {
+        [[unlikely]] if (!value)
+        {
+            return 0;
+        }
+
+        a9n::word rounded_value = round_up_power_of_2(value);
+
+        return calculate_radix(rounded_value);
+    }
+
+    inline constexpr a9n::word calculate_radix_floor(a9n::word value)
+    {
+        [[unlikely]] if (!value)
+        {
+            return 0;
+        }
+
+        a9n::word rounded_value = round_down_power_of_2(value);
+
+        return calculate_radix(rounded_value);
     }
 }
 
