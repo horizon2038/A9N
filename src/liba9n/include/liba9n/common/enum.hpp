64afd694499d75a5a4ea5acdf0af0d75c621a422
horizon <rekka728@gmail.com>
2024-11-30 14:34:43 +0900

    [update] type-safe enum cast

--- src/liba9n/include/liba9n/common/enum.hpp
+++ src/liba9n/include/liba9n/common/enum.hpp
@@ -7,7 +7,7 @@ namespace liba9n
 {
     template<typename Enum, typename EnumBase = liba9n::std::underlying_type_t<Enum>>
         requires(liba9n::std::is_enum_v<Enum>)
-    EnumBase enum_cast(Enum e)
+    constexpr EnumBase enum_cast(Enum e)
     {
         return static_cast<EnumBase>(e);
     }
