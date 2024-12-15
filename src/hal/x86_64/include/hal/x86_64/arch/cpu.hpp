9fe6910605ac0f8eecffac28f23837f96210a007
horizon <rekka728@gmail.com>
2024-12-15 23:43:34 +0900

    [add] HAL::x86_64 : arch cpu local variables

--- src/hal/x86_64/include/hal/x86_64/arch/cpu.hpp
+++ src/hal/x86_64/include/hal/x86_64/arch/cpu.hpp
@@ -1,7 +1,9 @@
-#ifndef X86_64_CPU_HPP
-#define X86_64_CPU_HPP
+#ifndef A9N_HAL_X86_64_ARCH_CPU_HPP
+#define A9N_HAL_X86_64_ARCH_CPU_HPP
 
-#include <hal/haL_result.hpp>
+#include <hal/interface/cpu.hpp>
+
+#include <hal/hal_result.hpp>
 #include <hal/x86_64/arch/segment_configurator.hpp>
 #include <hal/x86_64/interrupt/interrupt_descriptor.hpp>
 
@@ -10,74 +12,40 @@
 #include <liba9n/libcxx/array>
 #include <stdint.h>
 
-// HACK: fix
-extern "C" void _write_gs_base(void *gs_base_address);
+extern "C" void     _write_gs_base(uint64_t gs_base_address);
+extern "C" uint64_t _read_gs_base();
+
+extern "C" void     _write_fs_base(uint64_t fs_base_address);
+extern "C" uint64_t _read_fs_base();
 
 namespace a9n::hal::x86_64
 {
-    inline constexpr a9n::word CPU_COUNT_MAX         = 8;
-    inline constexpr a9n::word KERNEL_STACK_SIZE_MAX = 512;
-
-    using kernel_stack = liba9n::std::array<a9n::word, KERNEL_STACK_SIZE_MAX>;
-
-    // 64 : cpu cache line size
-    struct alignas(64) cpu_local_variable
+    struct arch_cpu_local_variable
     {
-        // kernel_stack_top is the top address of the stack.
-        // NOTE: that the value is pushed downwards.
-        kernel_stack             *kernel_stack_pointer;
-        kernel::hardware_context *current_context;
-        a9n::word                 core_number;
-        alignas(8) bool is_kernel_use_context;
+        uint64_t local_apic_id { 0 };
 
         // GDT (Global Descriptor Table), IDT (Interrupt Descriptor Table),
         // TSS (Task State Segment) is exists per processor-core.
-        global_descriptor_table gdt;
-        interrupt_descriptor_64 idt;
-        task_state_segment      tss;
+        global_descriptor_table    gdt;
+        interrupt_descriptor_table idt;
+        task_state_segment         tss;
     } __attribute__((packed));
 
-    namespace segment_selector
+    namespace cpu_local_variable_offset
     {
-        inline constexpr uint16_t KERNEL_STACK_POINTER_OFFSET = 0x00;
-        inline constexpr uint16_t USER_STACK_POINTER_OFFSET   = 0x08;
-        inline constexpr uint16_t GDT_OFFSET                  = 0x10;
-        inline constexpr uint16_t IDT_OFFSET
-            = sizeof(global_descriptor_table) + GDT_OFFSET;
-        inline constexpr uint16_t TSS_OFFSET
-            = sizeof(interrupt_descriptor_64) + IDT_OFFSET;
+        inline constexpr uint64_t GDT_OFFSET = 0x00;
+        inline constexpr uint64_t IDT_OFFSET = sizeof(global_descriptor_table) + GDT_OFFSET;
+        inline constexpr uint64_t TSS_OFFSET = sizeof(interrupt_descriptor_64) + IDT_OFFSET;
     }
 
-    // save to gs segment.
-    inline cpu_local_variable cpu_local_variables[CPU_COUNT_MAX];
+    // *x86_64 specific* cpu local variable
+    inline arch_cpu_local_variable arch_cpu_local_variables[a9n::kernel::CPU_COUNT_MAX];
 
-    // for interrupt/syscall.
-    alignas(KERNEL_STACK_SIZE_MAX
-    ) inline kernel_stack kernel_stacks[CPU_COUNT_MAX];
+    hal_result init_cpu_core(void);
+    hal_result init_cpu_core_features(void);
+    hal_result init_cpu_core_segments(void);
 
-    inline hal_result init_cpu_state(a9n::word core_number)
-    {
-        if (core_number >= CPU_COUNT_MAX)
-        {
-            return hal_error::ILLEGAL_ARGUMENT;
-        }
-
-        // WARN: *unsafe*
-        cpu_local_variables[core_number].kernel_stack_pointer
-            = reinterpret_cast<kernel_stack *>(&kernel_stacks[core_number])
-            + KERNEL_STACK_SIZE_MAX;
-        cpu_local_variables[core_number].core_number = core_number;
-
-        _write_gs_base(&(cpu_local_variables[0]));
-
-        return {};
-    }
-
-    inline a9n::word current_core_number()
-    {
-        // stub (single-core)
-        return 0;
-    }
-
+    liba9n::result<arch_cpu_local_variable *, hal_error> current_arch_local_variable(void);
 }
+
 #endif
