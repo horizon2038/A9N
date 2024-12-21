36f8cceb556151a4cd457570e91adbffe479a791
horizon <rekka728@gmail.com>
2024-12-22 00:40:48 +0900

    [update] uefi memory map buffer

--- src/boot/a9nloaderPkg/a9nloader/uefi_memory_map.c
+++ src/boot/a9nloaderPkg/a9nloader/uefi_memory_map.c
@@ -1,8 +1,14 @@
 #include "uefi_memory_map.h"
 
+static char memory_map_buffer[4096 * 8];
+
 EFI_STATUS get_uefi_memory_map(uefi_memory_map *target_memory_map)
 {
-    target_memory_map->map_size = target_memory_map->buffer_size;
+    target_memory_map->buffer      = (void *)memory_map_buffer;
+
+    target_memory_map->map_size    = sizeof(memory_map_buffer);
+    target_memory_map->buffer_size = sizeof(memory_map_buffer);
+
     return gBS->GetMemoryMap(
         &target_memory_map->map_size,
         target_memory_map->buffer,
