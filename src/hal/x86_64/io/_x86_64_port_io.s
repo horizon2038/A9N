0038a144a79dcb846e94c13bd3b0782ca33ed0ee
horizon <rekka728@gmail.com>
2025-08-21 12:39:28 +0900

    [fix] incorrect register specification for port in/out (hal::x86_64)

--- src/hal/x86_64/io/_x86_64_port_io.s
+++ src/hal/x86_64/io/_x86_64_port_io.s
@@ -3,16 +3,15 @@ global _port_read_8, _port_write_8
 global _port_read_32, _port_write_32
 
 _port_read_8:
-    mov rdx, rdi
-    in ax, dx
-    nop
+    mov dx, di
+    in al, dx
+    movzx eax, al
     ret
 
 _port_write_8:
-    mov rdx, rdi
-    mov rax, rsi
-    out dx, ax
-    nop
+    mov dx, di
+    mov al, sil 
+    out dx, al
     ret
 
 _port_read_32:
