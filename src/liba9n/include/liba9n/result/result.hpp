4f82eda1c04af47747736520a4c3b598ff021c0f
horizon <rekka728@gmail.com>
2026-05-14 18:09:15 +0900

    [add] TRY macro

--- src/liba9n/include/liba9n/result/result.hpp
+++ src/liba9n/include/liba9n/result/result.hpp
@@ -5,4 +5,23 @@
 #include <liba9n/result/__result/result_impl.hpp>
 #include <liba9n/result/__result/result_void_impl.hpp>
 
+#define TRY(expr) \
+    ({ \
+        auto &&__result = (expr); \
+        if (__result.is_error()) \
+        { \
+            return __result.unwrap_error(); \
+        } \
+        __result.unwrap(); \
+    })
+
+#define TRY_VOID(expr) \
+    ({ \
+        auto &&__result = (expr); \
+        if (__result.is_error()) \
+        { \
+            return __result.unwrap_error(); \
+        } \
+    })
+
 #endif
