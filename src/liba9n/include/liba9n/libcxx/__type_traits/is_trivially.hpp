926565dc90af2a48e054eea7da6f7e509552d330
horizon2038 <rekka728@gmail.com>
2026-08-10 01:58:25 +0900

    [fix] incorrect trivial judgment for assignment

--- src/liba9n/include/liba9n/libcxx/__type_traits/is_trivially.hpp
+++ src/liba9n/include/liba9n/libcxx/__type_traits/is_trivially.hpp
@@ -7,24 +7,19 @@
 namespace liba9n::std
 {
     template<typename T>
-    struct is_trivially_copyable
-        : integral_constant<bool, __is_trivially_copyable(T)>
+    struct is_trivially_copyable : integral_constant<bool, __is_trivially_copyable(T)>
     {
     };
 
     template<typename T>
     struct is_trivially_copy_constructible
-        : integral_constant<
-              bool,
-              __is_trivially_constructible(T, add_lvalue_reference_t<const T>)>
+        : integral_constant<bool, __is_trivially_constructible(T, add_lvalue_reference_t<const T>)>
     {
     };
 
     template<typename T>
     struct is_trivially_move_constructible
-        : integral_constant<
-              bool,
-              __is_trivially_constructible(T, add_rvalue_reference_t<const T>)>
+        : integral_constant<bool, __is_trivially_constructible(T, add_rvalue_reference_t<T>)>
     {
     };
 
@@ -32,29 +27,25 @@ namespace liba9n::std
     struct is_trivially_copy_assignable
         : integral_constant<
               bool,
-              __is_trivially_assignable(T, add_lvalue_reference_t<const T>)>
+              __is_trivially_assignable(add_lvalue_reference_t<T>, add_lvalue_reference_t<const T>)>
     {
     };
 
     template<typename T>
     struct is_trivially_move_assignable
-        : integral_constant<
-              bool,
-              __is_trivially_assignable(T, add_rvalue_reference_t<const T>)>
+        : integral_constant<bool, __is_trivially_assignable(add_lvalue_reference_t<T>, add_rvalue_reference_t<T>)>
     {
     };
 
 #if __has_builtin(__is_trivially_destructible)
     template<typename T>
-    struct is_trivially_destructible
-        : integral_constant<bool, __is_trivially_destructible(T)>
+    struct is_trivially_destructible : integral_constant<bool, __is_trivially_destructible(T)>
     {
     };
 
 #elif __has_builtin(__has_trivial_destructor)
     template<typename T>
-    struct is_trivially_destructible
-        : integral_constant<bool, __has_trivial_destructor(T)>
+    struct is_trivially_destructible : integral_constant<bool, __has_trivial_destructor(T)>
     {
     };
 
@@ -64,24 +55,19 @@ namespace liba9n::std
     inline constexpr bool is_trivially_copyable_v = is_trivially_copyable<T>::value;
 
     template<typename T>
-    inline constexpr bool is_trivially_copy_constructible_v
-        = is_trivially_copy_constructible<T>::value;
+    inline constexpr bool is_trivially_copy_constructible_v = is_trivially_copy_constructible<T>::value;
 
     template<typename T>
-    inline constexpr bool is_trivially_move_constructible_v
-        = is_trivially_move_constructible<T>::value;
+    inline constexpr bool is_trivially_move_constructible_v = is_trivially_move_constructible<T>::value;
 
     template<typename T>
-    inline constexpr bool is_trivially_copy_assignable_v
-        = is_trivially_copy_assignable<T>::value;
+    inline constexpr bool is_trivially_copy_assignable_v = is_trivially_copy_assignable<T>::value;
 
     template<typename T>
-    inline constexpr bool is_trivially_move_assignable_v
-        = is_trivially_move_assignable<T>::value;
+    inline constexpr bool is_trivially_move_assignable_v = is_trivially_move_assignable<T>::value;
 
     template<typename T>
-    inline constexpr bool is_trivially_destructible_v
-        = is_trivially_destructible<T>::value;
+    inline constexpr bool is_trivially_destructible_v = is_trivially_destructible<T>::value;
 }
 
 #endif
